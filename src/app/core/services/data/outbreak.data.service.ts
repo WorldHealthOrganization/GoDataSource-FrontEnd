78447a62ee5d8e08135b39495a577dca4917b974
Mihai Ilie <mihai.ilie@clarisoft.com>
2018-05-25 19:50:46 +0300

    WW-48 [GD-FD_2] - ST18 - All the data that is recorded into the application will always be linked to the active outbreak without having to select the outbreak. If someone wants to make changes to a record, they need to switch to the respective outbreak and...

--- src/app/core/services/data/outbreak.data.service.ts
+++ src/app/core/services/data/outbreak.data.service.ts
@@ -4,16 +4,21 @@ import { Observable } from 'rxjs/Observable';
 import { ObservableHelperService } from '../helper/observable-helper.service';
 
 import 'rxjs/add/operator/map';
-import {OutbreakModel} from "../../models/outbreak.model";
-import { UserRoleModel } from "../../models/user-role.model";
+import { OutbreakModel } from '../../models/outbreak.model';
+import { UserRoleModel } from '../../models/user-role.model';
+import { StorageKey, StorageService } from '../helper/storage.service';
 
 
 @Injectable()
 export class OutbreakDataService {
 
+    // an Outbreak is active across the application
+    activeOutbreak: OutbreakModel;
+
     constructor(
         private http: HttpClient,
-        private observableHelper: ObservableHelperService
+        private observableHelper: ObservableHelperService,
+        private storageService: StorageService
     ) {
     }
 
@@ -65,6 +70,50 @@ export class OutbreakDataService {
      */
     modifyOutbreak(outbreakId: string, data: any): Observable<any> {
         return this.http.patch(`outbreaks/${outbreakId}`, data);
+    }
+
+    /**
+     * Get the outfit that is Active across the application
+     * @returns {OutbreakModel}
+     */
+    getActiveOutbreak(): Observable<OutbreakModel> {
+        // get the Active Outbreak from cache
+        if (this.activeOutbreak) {
+            return Observable.of(this.activeOutbreak);
+        }
+
+        // get the Active Outbreak from local storage
+        const activeOutbreakFromStorage = <OutbreakModel>this.storageService.get(StorageKey.ACTIVE_OUTBREAK);
+        if (activeOutbreakFromStorage) {
+            // cache the Active Outbreak
+            this.activeOutbreak = activeOutbreakFromStorage;
+            // ...and return it
+            return Observable.of(this.activeOutbreak);
+        }
+
+        // get the first Outbreak from the Outbreaks list
+        return this.getOutbreaksList()
+            .map((outbreaksList) => {
+                if (outbreaksList.length > 0) {
+                    // get the first outbreak
+                    const activeOutbreak = <OutbreakModel>outbreaksList[0];
+
+                    // cache the outbreak
+                    this.activeOutbreak = activeOutbreak;
+
+                    // set the outbreak in local storage
+                    this.storageService.set(StorageKey.ACTIVE_OUTBREAK, activeOutbreak);
+
+                    // ...and return it
+                    return activeOutbreak;
+                } else {
+                    return null;
+                }
+            });
+    }
+
+    setActiveOutbreak() {
+
     }
 
 }
--- src/app/shared/components/topnav/topnav.component.ts
+++ src/app/shared/components/topnav/topnav.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { OutbreakDataService } from '../../../core/services/data/outbreak.data.service';
+import { OutbreakModel } from '../../../core/models/outbreak.model';
 
 @Component({
     selector: 'app-topnav',
@@ -10,5 +12,14 @@ export class TopnavComponent {
 
     // by default, do nothing (stay on the current page)
     @Input() addNewItemRoute = '.';
+
+    // the Active Outbreak
+    activeOutbreak: OutbreakModel = new OutbreakModel();
+
+    constructor(
+        private outbreakDataService: OutbreakDataService
+    ) {
+        // get the active outbreak
+    }
 
 }
