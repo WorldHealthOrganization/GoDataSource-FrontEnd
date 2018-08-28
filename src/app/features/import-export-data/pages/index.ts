b56d38704b6780f0679707d67ceea24af7f0ae23
Adrian Morariu <adrian.morariu@clarisoft.com>
2018-08-28 18:50:53 +0300

    WGD-444 ST1: As a Go.Data user, I want to be able to import an hierarchical list of locations

--- src/app/features/import-export-data/pages/index.ts
+++ src/app/features/import-export-data/pages/index.ts
@@ -1,13 +1,16 @@
 // import-data each page component
 import { ImportHierarchicalLocationsComponent } from './import-hierarchical-locations/import-hierarchical-locations.component';
 import { ImportCaseLabDataComponent } from './import-case-lab-data/import-case-lab-data.component';
+import { ImportDataComponent } from '../components/import-data/import-data.component';
 
 // export each page component individually
 export * from './import-hierarchical-locations/import-hierarchical-locations.component';
 export * from './import-case-lab-data/import-case-lab-data.component';
+export * from '../components/import-data/import-data.component';
 
 // export the list of all page components
 export const pageComponents: any[] = [
+    ImportDataComponent,
     ImportHierarchicalLocationsComponent,
     ImportCaseLabDataComponent
 ];
