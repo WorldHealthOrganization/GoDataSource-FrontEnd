46b19c1ac07bcbb1899ae1ee77607b8bffb71ef1
Adrian Morariu <adrian.morariu@clarisoft.com>
2018-08-30 16:33:01 +0300

    WGD-266 ST8: As a Go.Data user I want to be able to import lab results en masse from a structured source

---
+++ src/app/core/services/data/import-export.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class ImportExportService {
+
+    constructor(
+        private http: HttpClient
+    ) {}
+
+    /**
+     * Import data
+     * @param url
+     * @param data
+     */
+    importData(url: string, data: {}): Observable<any> {
+        return this.http.post(url, data);
+    }
+}
+
