c5de7f047042f8c6c4af9bd75af3709a8617a54f
George Craciun <george.craciun@clarisoft.com>
2018-09-26 15:35:11 +0300

    WGD-295 ST11: As a Go.Data user I want to be able to view the size of chains of transmission

--- src/app/features/dashboard/components/transmission-chains-dashlet/transmission-chains-dashlet.component.ts
+++ src/app/features/dashboard/components/transmission-chains-dashlet/transmission-chains-dashlet.component.ts
@@ -36,6 +36,89 @@ export class TransmissionChainsDashletComponent implements OnInit {
     caseClassificationsList$: Observable<any[]>;
     genderList$: Observable<any[]>;
 
+
+
+
+    view: any[] = [700, 400];
+
+    // options
+    showXAxis = true;
+    showYAxis = true;
+    gradient = false;
+    showLegend = true;
+    showXAxisLabel = true;
+    xAxisLabel = 'Country';
+    showYAxisLabel = true;
+    yAxisLabel = 'Population';
+
+    colorScheme = {
+        domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+    };
+
+    single = [
+        {
+            "name": "Germany",
+            "value": 8940000
+        },
+        {
+            "name": "USA",
+            "value": 5000000
+        },
+        {
+            "name": "France",
+            "value": 7200000
+        }
+    ];
+
+    multi = [
+        {
+            "name": "Germany",
+            "series": [
+                {
+                    "name": "2010",
+                    "value": 7300000
+                },
+                {
+                    "name": "2011",
+                    "value": 8940000
+                }
+            ]
+        },
+
+        {
+            "name": "USA",
+            "series": [
+                {
+                    "name": "2010",
+                    "value": 7870000
+                },
+                {
+                    "name": "2011",
+                    "value": 8270000
+                }
+            ]
+        },
+
+        {
+            "name": "France",
+            "series": [
+                {
+                    "name": "2010",
+                    "value": 5000002
+                },
+                {
+                    "name": "2011",
+                    "value": 5800000
+                }
+            ]
+        }
+    ];
+
+
+    onSelectChart(event) {
+        console.log(event);
+    }
+
     constructor(
         private outbreakDataService: OutbreakDataService,
         private transmissionChainDataService: TransmissionChainDataService,
