72e0b9abd14be90b5a877619330d5a429820a7e6
Adrian Morariu <adrian.morariu@clarisoft.com>
2018-10-12 10:19:40 +0300

    WGD-786 Implement requirement "CFU_VISU15_104 The system must highlight any inconsistencies in key dates entered (onset, report, outcome)."

--- src/app/features/outbreak/pages/index.ts
+++ src/app/features/outbreak/pages/index.ts
@@ -2,20 +2,12 @@
 import { OutbreakListComponent } from './outbreak-list/outbreak-list.component';
 import { CreateOutbreakComponent } from './create-outbreak/create-outbreak.component';
 import { ModifyOutbreakComponent } from './modify-outbreak/modify-outbreak.component';
-import { FormQuestionListComponent } from '../components/form-question-list/form-question-list.component';
-import { FormAnswerListComponent } from '../components/form-answer-list/form-answer-list.component';
-import { FormSubQuestionListComponent } from '../components/form-sub-question-list/form-sub-question-list.component';
-import { FormSubAnswerListComponent } from '../components/form-sub-answer-list/form-sub-answer-list.component';
 import { InconsistenciesListComponent } from './inconsistencies-list/inconsistencies-list.component';
 
 // export each page component individually
 export * from './outbreak-list/outbreak-list.component';
 export * from './create-outbreak/create-outbreak.component';
 export * from './modify-outbreak/modify-outbreak.component';
-export * from '../components/form-answer-list/form-answer-list.component';
-export * from '../components/form-question-list/form-question-list.component';
-export * from '../components/form-sub-question-list/form-sub-question-list.component';
-export * from '../components/form-sub-answer-list/form-sub-answer-list.component';
 export * from './inconsistencies-list/inconsistencies-list.component';
 
 // export the list of all page components
@@ -24,13 +16,5 @@ export const pageComponents: any[] = [
     ModifyOutbreakComponent,
     OutbreakListComponent,
 
-    InconsistenciesListComponent,
-
-    // question templates
-    FormQuestionListComponent,
-    FormAnswerListComponent,
-
-    // needed for fixing circular dependencies
-    FormSubQuestionListComponent,
-    FormSubAnswerListComponent
+    InconsistenciesListComponent
 ];
