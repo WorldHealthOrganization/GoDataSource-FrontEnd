c3adc023fd2e76fd2e08a5107c1749f16fd9d4dc
Andrei.Frincu <andrei.frincu@clarisoft.com>
2018-10-16 17:54:58 +0300

    WGD-1064 Case UI changes

--- src/app/features/case/pages/create-case/create-case.component.ts
+++ src/app/features/case/pages/create-case/create-case.component.ts
@@ -73,10 +73,6 @@ export class CreateCaseComponent extends ConfirmOnFormChanges implements OnInit 
 
         // by default, enforce Case having an address
         this.caseData.addresses.push(new AddressModel());
-        // ...and a hospitalization date range
-        this.caseData.hospitalizationDates.push(new DateRangeModel());
-        // ...and an isolation date range
-        this.caseData.isolationDates.push(new DateRangeModel());
 
         // get selected outbreak
         this.outbreakDataService
