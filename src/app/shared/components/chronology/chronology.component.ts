c71220f81e0718d0c4cd9eebb214306110742aff
Adrian Morariu <adrian.morariu@clarisoft.com>
2018-10-17 16:54:32 +0300

    WGD-1086 Cases and Contacts Chronology updates

--- src/app/shared/components/chronology/chronology.component.ts
+++ src/app/shared/components/chronology/chronology.component.ts
@@ -1,4 +1,21 @@
 import { Component, Input, ViewEncapsulation } from '@angular/core';
+import * as _ from 'lodash';
+
+export class ChronologyItem {
+    public label: string;
+    public date: string;
+
+    constructor(data: {
+        label: string,
+        date: string
+    }) {
+        // assign properties
+        Object.assign(
+            this,
+            data
+        );
+    }
+}
 
 @Component({
     selector: 'app-chronology',
@@ -7,7 +24,18 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
     styleUrls: ['./chronology.component.less']
 })
 export class ChronologyComponent {
+    private _entries: ChronologyItem[] = [];
+    @Input() set entries(entries: ChronologyItem[]) {
+        // set collection
+        this._entries = entries;
 
-    @Input() entries: any[] = [];
-
+        // sort collection asc
+        this._entries = _.sortBy(
+            this._entries,
+            'date'
+        );
+    }
+    get entries(): ChronologyItem[] {
+        return this._entries;
+    }
 }
