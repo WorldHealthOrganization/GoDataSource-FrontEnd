93d81674bd8568776cd5f63e77d66cd4a69c3cc3
George Craciun <george.craciun@clarisoft.com>
2018-10-18 14:23:02 +0300

    WGD-1111 Dashboard: Pie chart: Case Summary

--- src/app/features/dashboard/components/index.ts
+++ src/app/features/dashboard/components/index.ts
@@ -16,7 +16,7 @@ import { NewChainsOfTransmissionFromRegisteredContactsDashletComponent } from '.
 import { ContactsSeenEachDayDashletComponent } from './contacts-seen-each-day-dashlet/contacts-seen-each-day-dashlet.component';
 import { ContactsWithSuccessfulFollowUpsDashletComponent } from './contacts-with-successful-follow-ups-dashlet/contacts-with-successful-follow-ups-dashlet.component';
 import { HistogramTransmissionChainsSizeDashletComponent } from './histogram-transmission-chains-size-dashlet/histogram-transmission-chains-size-dashlet.component';
-import { CaseSummaryComponent } from './case-summary-dashlet/case-summary.component';
+import { CaseSummaryDashletComponent } from './case-summary-dashlet/case-summary-dashlet.component';
 
 // export the list of all page components
 export const components: any[] = [
@@ -37,6 +37,6 @@ export const components: any[] = [
     ContactsSeenEachDayDashletComponent,
     ContactsWithSuccessfulFollowUpsDashletComponent,
     HistogramTransmissionChainsSizeDashletComponent,
-    CaseSummaryComponent
+    CaseSummaryDashletComponent
 ];
 
