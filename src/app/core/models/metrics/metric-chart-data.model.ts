a225ad8c4ac057f7671a676eebec439909573c9a
George Craciun <george.craciun@clarisoft.com>
2018-10-24 16:51:47 +0300

    WGD-314

--- src/app/core/models/metrics/metric-chart-data.model.ts
+++
@@ -1,9 +0,0 @@
-import * as _ from 'lodash';
-export class MetricChartDataModel {
-    name: string;
-    value: number;
-    constructor(data = null) {
-        this.name = _.get(data, 'name', '');
-        this.value = _.get(data, 'value', 0);
-    }
-}
