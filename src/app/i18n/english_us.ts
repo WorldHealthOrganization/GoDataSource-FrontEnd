6d479d608059842e80f67d8c8f752adc7c859e49
Andrei.Frincu <andrei.frincu@clarisoft.com>
2019-01-15 16:25:31 +0200

    WGD-1095 Add new "targeted" field on Follow Up model

--- src/app/i18n/english_us.ts
+++ src/app/i18n/english_us.ts
@@ -12,5 +12,14 @@ export const EnglishUsLang = {
          */
         // 'LNG_EXAMPLE_TOKEN': 'Example value',
 
+        /**
+         * Follow Up Fields
+         */
+        'LNG_FOLLOW_UP_FIELD_LABEL_TARGETED': 'Targeted',
+
+        /**
+         * Follow Ups List Page
+         */
+        'LNG_PAGE_LIST_FOLLOW_UPS_ACTION_CHANGE_TARGETED_STATUS_SUCCESS_MESSAGE': 'Targeted status changed!',
     }
 };
