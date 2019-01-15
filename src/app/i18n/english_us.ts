7d4e61cac034a058c9c0208c55f0f83d36df05fb
Andrei.Frincu <andrei.frincu@clarisoft.com>
2019-01-15 18:33:52 +0200

    WGD-1100 Display explicit labels for action buttons

--- src/app/i18n/english_us.ts
+++ src/app/i18n/english_us.ts
@@ -12,5 +12,28 @@ export const EnglishUsLang = {
          */
         // 'LNG_EXAMPLE_TOKEN': 'Example value',
 
+        /**
+         * Outbreaks List Page
+         */
+        'LNG_PAGE_LIST_OUTBREAKS_ACTION_VIEW_OUTBREAK_BUTTON': 'View Outbreak',
+        'LNG_PAGE_LIST_OUTBREAKS_ACTION_CLONE_OUTBREAK_BUTTON': 'Clone Outbreak',
+        'LNG_PAGE_LIST_OUTBREAKS_ACTION_MODIFY_OUTBREAK_BUTTON': 'Modify Outbreak',
+        'LNG_PAGE_LIST_OUTBREAKS_ACTION_DELETE_OUTBREAK_BUTTON': 'Delete Outbreak',
+        'LNG_PAGE_LIST_OUTBREAKS_ACTION_RESTORE_OUTBREAK_BUTTON': 'Restore Outbreak',
+
+        /**
+         * Outbreak Template List Page
+         */
+
+        'LNG_PAGE_LIST_OUTBREAK_TEMPLATE_ACTION_VIEW_OUTBREAK_TEMPLATE_BUTTON': 'View Outbreak Template',
+        'LNG_PAGE_LIST_OUTBREAK_TEMPLATE_ACTION_MODIFY_OUTBREAK_TEMPLATE_BUTTON': 'Modify Outbreak Template',
+        'LNG_PAGE_LIST_OUTBREAK_TEMPLATE_ACTION_DELETE_OUTBREAK_TEMPLATE_BUTTON': 'Delete Outbreak Template',
+
+        /**
+         * Cases List Page
+         */
+        'LNG_PAGE_LIST_CASES_ACTION_VIEW_CASE_BUTTON': 'View Case',
+        'LNG_PAGE_LIST_CASES_ACTION_MODIFY_CASE_BUTTON': 'Modify Case',
+        'LNG_PAGE_LIST_CASES_ACTION_DELETE_CASE_BUTTON': 'Delete Case',
     }
 };
