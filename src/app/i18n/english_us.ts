62de51247e776362c07d104f8693f4dfe7236639
Andrei.Frincu <andrei.frincu@clarisoft.com>
2019-01-25 18:04:28 +0200

    WGD-1531 Modify Person Address: Have a flag that shows if coordinates are accurate

--- src/app/i18n/english_us.ts
+++ src/app/i18n/english_us.ts
@@ -269,6 +269,7 @@ export const EnglishUsLang = {
         'LNG_DIALOG_CONFIRM_DELETE_NEW_QUESTION_ANSWER': 'Since this is a new answer, it will be removed if you cancel. Do you want to continue?',
         'LNG_DIALOG_CONFIRM_LOOSE_CHANGES_QUESTION': 'There are unsaved changes that will be lost if you continue',
         'LNG_DIALOG_CONFIRM_LOOSE_CHANGES_QUESTION_ANSWER': 'There are unsaved changes that will be lost if you continue',
+        'LNG_DIALOG_CONFIRM_REPLACE_GEOLOCATION': `Do you want to replace current latitude and longitude with location's values? If you enter the coordinates manually you should turn the slider on.`,
 
         /**
          * uiFormInputValidationErrors
@@ -324,7 +325,13 @@ export const EnglishUsLang = {
         /**
          * uiEntityFields
          */
-        'LNG_ENTITY_FIELD_LABEL_VISUAL_ID': 'Case / Contact ID'
+        'LNG_ENTITY_FIELD_LABEL_VISUAL_ID': 'Case / Contact ID',
+
+        /**
+         * uiAddressFields
+         */
+        'LNG_ADDRESS_FIELD_LABEL_MANUAL_COORDINATES': 'Coordinates were entered manually',
+        'LNG_ADDRESS_FIELD_LABEL_MANUAL_COORDINATES_DESCRIPTION': '',
 
 
         // some tokens might need to be removed
