6765c8478142b0945645003bb9e63dcdb98b570b
Andrei.Frincu <andrei.frincu@clarisoft.com>
2019-02-18 14:06:41 +0200

    WGD-1737 Launch app in electron web browser window

--- src/app/features/transmission-chain/pages/case-count-map/case-count-map.component.ts
+++ src/app/features/transmission-chain/pages/case-count-map/case-count-map.component.ts
@@ -8,19 +8,12 @@ import { CaseModel } from '../../../../core/models/case.model';
 import { AddressType } from '../../../../core/models/address.model';
 import { WorldMapComponent, WorldMapMarker, WorldMapMarkerLayer, WorldMapPoint } from '../../../../shared/components/world-map/world-map.component';
 import * as _ from 'lodash';
-import { Observable } from 'rxjs/Observable';
-import { Subscriber } from 'rxjs/Subscriber';
 import { BreadcrumbItemModel } from '../../../../shared/components/breadcrumbs/breadcrumb-item.model';
 import { Subscription } from 'rxjs/Subscription';
-import {
-    DialogAnswerButton, DialogConfiguration,
-    DialogField
-} from '../../../../shared/components/dialog/dialog.component';
 import { DialogService } from '../../../../core/services/helper/dialog.service';
 import { ImportExportDataService } from '../../../../core/services/data/import-export.data.service';
 import { I18nService } from '../../../../core/services/helper/i18n.service';
 import * as FileSaver from 'file-saver';
-
 
 @Component({
     selector: 'app-case-count-map',
@@ -86,37 +79,23 @@ export class CaseCountMapComponent implements OnInit, OnDestroy {
         }
     }
 
+    /**
+     * Export case count map
+     */
     exportCaseCountMap() {
-        this.dialogService.showInput(
-            new DialogConfiguration({
-                message: 'LNG_DIALOG_CONFIRM_EXPORT_CHAINS_OF_TRANSMISSION',
-                yesLabel: 'LNG_DIALOG_CONFIRM_BUTTON_YES',
-                required: true,
-                fieldsList: [new DialogField({
-                    name: 'splitFactor',
-                    placeholder: 'LNG_PAGE_GRAPH_CHAINS_OF_TRANSMISSION_EXPORT_SPLIT_FACTOR',
-                    required: true,
-                    type: 'number',
-                    value: '1'
-                })],
-            }), true)
-            .subscribe((answer) => {
-                if (answer.button === DialogAnswerButton.Yes) {
-                    // get the chosen split factor
-                    const splitFactor = answer.inputValue.value.splitFactor;
-                    // get the base64 png
-                    const pngBase64 = this.worldMap.getPng64();
-                    // cal the api for the pdf
-                    this.importExportDataService.exportImageToPdf({image: pngBase64, responseType: 'blob', splitFactor: Number(splitFactor)})
-                        .subscribe((blob) => {
-                           const fileName = this.i18nService.instant('token');
-                           FileSaver.saveAs(
-                               blob,
-                               `${fileName}`
-                           );
-                        });
-                }
-            });
+        if (this.worldMap) {
+            const loadingDialog = this.dialogService.showLoadingDialog();
+            this.worldMap
+                .printToBlob()
+                .subscribe((blob) => {
+                    const fileName = this.i18nService.instant('LNG_PAGE_CASE_COUNT_TITLE');
+                    FileSaver.saveAs(
+                        blob,
+                        `${fileName}.png`
+                    );
+                    loadingDialog.close();
+                });
+        }
     }
 
     /**
@@ -171,24 +150,5 @@ export class CaseCountMapComponent implements OnInit, OnDestroy {
                     this.displayLoading = false;
                 });
         }
-    }
-
-    /**
-     * Print to blob
-     */
-    printToBlob(): Observable<Blob> {
-        return Observable.create((observer: Subscriber<Blob>) => {
-            if (this.worldMap) {
-                this.worldMap
-                    .printToBlob()
-                    .subscribe((blob) => {
-                        observer.next(blob);
-                        observer.complete();
-                    });
-            } else {
-                observer.next(null);
-                observer.complete();
-            }
-        });
     }
 }
