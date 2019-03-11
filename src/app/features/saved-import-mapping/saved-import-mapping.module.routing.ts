0323bc54a718caf6285a408eb148f20c6370d9c1
Andrei.Frincu <andrei.frincu@clarisoft.com>
2019-03-11 16:03:45 +0200

    WGD-1677 Save import mapping per user

--- src/app/features/saved-import-mapping/saved-import-mapping.module.routing.ts
+++ src/app/features/saved-import-mapping/saved-import-mapping.module.routing.ts
@@ -1,6 +1,6 @@
 import { RouterModule, Routes } from '@angular/router';
 
-import * as fromPages from  './pages';
+import * as fromPages from './pages';
 import { ModuleWithProviders } from '@angular/compiler/src/core';
 
 const routes: Routes = [
