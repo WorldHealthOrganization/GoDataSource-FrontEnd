16df51faa69e9c9cc417c343371d3ac414de188c
George Craciun <george.craciun@clarisoft.com>
2019-03-21 13:22:40 +0200

    WGD-1727

--- src/app/i18n/english_us.ts
+++ src/app/i18n/english_us.ts
@@ -29,9 +29,6 @@ export const EnglishUsLang = {
         'LNG_PAGE_DASHBOARD_CONTACT_NOT_PERCENTAGE_Y_AXIS_LABEL': 'Number of Contacts',
         'LNG_PAGE_DASHBOARD_CONTACT_NOT_PERCENTAGE_Y2_AXIS_LABEL': 'Percentage',
 
-
-
-
         /**
          * uiDialogs
          */
