b9082cb7fc028e21c789ea5fb82aa9c7dc6d4bb9
Andrei.Frincu <andrei.frincu@clarisoft.com>
2019-04-15 17:07:55 +0300

    WGD-2131 View/Edit Lab Result Screen Missing Case Name

--- src/app/i18n/english_us.ts
+++ src/app/i18n/english_us.ts
@@ -39,6 +39,12 @@ export const EnglishUsLang = {
          */
         'LNG_FOLLOW_UP_FIELD_LABEL_NOT_TARGETED': 'Not targeted',
         'LNG_FOLLOW_UP_FIELD_LABEL_NOT_TARGETED_DESCRIPTION': '',
+
+        /**
+         * uiModifyCaseLabResultPage
+         */
+        'LNG_PAGE_VIEW_CASE_LAB_RESULT_FIELD_LABEL_CASE': 'Case'
+
         /**
          * REMOVE the tokens from below
          */
