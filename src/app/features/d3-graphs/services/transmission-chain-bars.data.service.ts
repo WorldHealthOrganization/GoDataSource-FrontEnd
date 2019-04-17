f653dac89c8cca0302dad181f0bb08d638463dfd
Mihai Ilie <mihai.ilie@clarisoft.com>
2019-04-17 14:30:10 +0300

    WGD-2053 Validation on hospitalization block start date

--- src/app/features/d3-graphs/services/transmission-chain-bars.data.service.ts
+++ src/app/features/d3-graphs/services/transmission-chain-bars.data.service.ts
@@ -3,11 +3,6 @@ import { HttpClient } from '@angular/common/http';
 import { RequestQueryBuilder } from '../../../core/helperClasses/request-query-builder';
 import { Observable } from 'rxjs';
 import { TransmissionChainBarsModel } from '../typings/transmission-chain-bars.model';
-import { map } from 'rxjs/operators';
-import { CaseBarModel } from '../typings/case-bar.model';
-import * as _ from 'lodash';
-import * as moment from 'moment';
-import { Constants } from '../../../core/models/constants';
 
 @Injectable()
 export class TransmissionChainBarsDataService {
@@ -24,36 +19,6 @@ export class TransmissionChainBarsDataService {
     ): Observable<TransmissionChainBarsModel> {
         const filter = queryBuilder.buildQuery();
 
-        return this.http.get(`outbreaks/${outbreakId}/cases/bars-transmission-chains?filter=${filter}`)
-            .pipe(map((data: TransmissionChainBarsModel) => {
-                // #TODO remove when API is fixed
-
-                // determine minGraphDate
-                let minGraphDate = data.minGraphDate;
-
-                // determine firstGraphDate for each Case
-                for (const key in data.casesMap) {
-                    const caseData = data.casesMap[key] as CaseBarModel;
-                    let firstGraphDate = caseData.dateOfOnset;
-
-                    _.each(caseData.dateRanges, (dateRange) => {
-                        firstGraphDate = moment(dateRange.startDate).format(Constants.DEFAULT_DATE_DISPLAY_FORMAT) < moment(firstGraphDate).format(Constants.DEFAULT_DATE_DISPLAY_FORMAT) ? dateRange.startDate : firstGraphDate;
-                    });
-
-                    _.each(caseData.labResults, (labResult) => {
-                        firstGraphDate = moment(labResult.dateSampleTaken).format(Constants.DEFAULT_DATE_DISPLAY_FORMAT) < moment(firstGraphDate).format(Constants.DEFAULT_DATE_DISPLAY_FORMAT) ? labResult.dateSampleTaken : firstGraphDate;
-                    });
-
-                    data.casesMap[key].firstGraphDate = firstGraphDate;
-
-                    if (moment(firstGraphDate).format(Constants.DEFAULT_DATE_DISPLAY_FORMAT) < moment(minGraphDate).format(Constants.DEFAULT_DATE_DISPLAY_FORMAT)) {
-                        minGraphDate = firstGraphDate;
-                    }
-                }
-
-                data.minGraphDate = minGraphDate;
-
-                return data;
-            })) as Observable<TransmissionChainBarsModel>;
+        return this.http.get(`outbreaks/${outbreakId}/cases/bars-transmission-chains?filter=${filter}`) as Observable<TransmissionChainBarsModel>;
     }
 }
