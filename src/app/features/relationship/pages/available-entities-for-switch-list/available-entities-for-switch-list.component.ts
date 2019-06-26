e0ba5115f90953f2e01ea2ccc1d69278cd800d7f
Andrei.Frincu <andrei.frincu@clarisoft.com>
2019-06-26 16:19:06 +0300

    WGD-2356 Bulk change source/contact of relationships

---
+++ src/app/features/relationship/pages/available-entities-for-switch-list/available-entities-for-switch-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-available-entities-for-switch-list',
+  templateUrl: './available-entities-for-switch-list.component.html',
+  styleUrls: ['./available-entities-for-switch-list.component.less']
+})
+export class AvailableEntitiesForSwitchListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
