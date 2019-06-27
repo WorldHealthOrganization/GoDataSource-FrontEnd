f66d70ac21c66957e14af32006537c9ddf3fab39
Andrei.Frincu <andrei.frincu@clarisoft.com>
2019-06-27 10:43:13 +0300

    WGD-2356 Bulk change source/contact of relationships

--- src/app/features/relationship/pages/available-entities-for-switch-list/available-entities-for-switch-list.component.ts
+++ src/app/features/relationship/pages/available-entities-for-switch-list/available-entities-for-switch-list.component.ts
@@ -10,9 +10,19 @@ import { Observable } from 'rxjs/index';
 import { CaseModel } from '../../../../core/models/case.model';
 import { ContactModel } from '../../../../core/models/contact.model';
 import { EventModel } from '../../../../core/models/event.model';
-import { share, tap } from 'rxjs/internal/operators';
+import { map, share, tap } from 'rxjs/internal/operators';
 import * as _ from 'lodash';
 import { RelationshipDataService } from '../../../../core/services/data/relationship.data.service';
+import { FilterModel, FilterType } from '../../../../shared/components/side-filters/model';
+import { Constants } from '../../../../core/models/constants';
+import { GenericDataService } from '../../../../core/services/data/generic.data.service';
+import { ReferenceDataDataService } from '../../../../core/services/data/reference-data.data.service';
+import {
+    ReferenceDataCategory, ReferenceDataCategoryModel,
+    ReferenceDataEntryModel
+} from '../../../../core/models/reference-data.model';
+import { LabelValuePair } from '../../../../core/models/label-value-pair';
+import { EntityType } from '../../../../core/models/entity-type';
 
 @Component({
     selector: 'app-available-entities-for-switch-list',
@@ -26,6 +36,24 @@ export class AvailableEntitiesForSwitchListComponent extends RelationshipsListCo
     entitiesList$: Observable<(CaseModel|ContactModel|EventModel)[]>;
     entitiesListCount$: Observable<any>;
 
+    // available side filters
+    availableSideFilters: FilterModel[];
+
+    // saved filters type
+    savedFiltersType = Constants.APP_PAGE.AVAILABLE_ENTITIES_FOR_RELATIONSHIPS.value;
+
+    // reference data
+    genderList$: Observable<any[]>;
+    personTypesList$: Observable<any[]>;
+    personTypesListMap: { [id: string]: ReferenceDataEntryModel };
+    riskLevelsList$: Observable<any[]>;
+    caseClassificationsList$: Observable<any[]>;
+
+    // provide constants to template
+    Constants = Constants;
+    ReferenceDataCategory = ReferenceDataCategory;
+    EntityType = EntityType;
+
     constructor(
         protected snackbarService: SnackbarService,
         protected router: Router,
@@ -33,7 +61,9 @@ export class AvailableEntitiesForSwitchListComponent extends RelationshipsListCo
         protected authDataService: AuthDataService,
         protected outbreakDataService: OutbreakDataService,
         protected entityDataService: EntityDataService,
-        private relationshipDataService: RelationshipDataService
+        private relationshipDataService: RelationshipDataService,
+        private genericDataService: GenericDataService,
+        private referenceDataDataService: ReferenceDataDataService,
     ) {
         super(
             snackbarService, router, route,
@@ -42,6 +72,34 @@ export class AvailableEntitiesForSwitchListComponent extends RelationshipsListCo
     }
 
     ngOnInit() {
+        super.ngOnInit();
+
+        // reference data
+        this.genderList$ = this.referenceDataDataService.getReferenceDataByCategoryAsLabelValue(ReferenceDataCategory.GENDER).pipe(share());
+        this.riskLevelsList$ = this.referenceDataDataService.getReferenceDataByCategoryAsLabelValue(ReferenceDataCategory.RISK_LEVEL).pipe(share());
+        this.caseClassificationsList$ = this.referenceDataDataService.getReferenceDataByCategoryAsLabelValue(ReferenceDataCategory.CASE_CLASSIFICATION).pipe(share());
+        const personTypes$ = this.referenceDataDataService.getReferenceDataByCategory(ReferenceDataCategory.PERSON_TYPE).pipe(share());
+        this.personTypesList$ = personTypes$
+            .pipe(
+                map((data: ReferenceDataCategoryModel) => {
+                    return _.map(data.entries, (entry: ReferenceDataEntryModel) =>
+                        new LabelValuePair(entry.value, entry.id)
+                    );
+                })
+            );
+        personTypes$.subscribe((personTypeCategory: ReferenceDataCategoryModel) => {
+            this.personTypesListMap = _.transform(
+                personTypeCategory.entries,
+                (result, entry: ReferenceDataEntryModel) => {
+                    // groupBy won't work here since groupBy will put an array instead of one value
+                    result[entry.id] = entry;
+                },
+                {}
+            );
+        });
+
+        // side filters
+        this.generateSideFilters();
     }
 
     /**
@@ -51,13 +109,13 @@ export class AvailableEntitiesForSwitchListComponent extends RelationshipsListCo
         // initialize breadcrumbs
         this.initializeBreadcrumbs();
 
-        // // initialize query builder
-        // this.clearQueryBuilder();
-        //
-        // // initialize pagination
-        // this.initPaginator();
-        // // ...and (re)load the list
-        // this.needsRefreshList(true);
+        // initialize query builder
+        this.clearQueryBuilder();
+
+        // initialize pagination
+        this.initPaginator();
+        // ...and (re)load the list
+        this.needsRefreshList(true);
     }
 
     /**
@@ -137,6 +195,67 @@ export class AvailableEntitiesForSwitchListComponent extends RelationshipsListCo
                 )
                 .pipe(share());
         }
+    }
+
+    private generateSideFilters() {
+        this.availableSideFilters = [
+            new FilterModel({
+                fieldName: 'type',
+                fieldLabel: 'LNG_ENTITY_FIELD_LABEL_TYPE',
+                type: FilterType.MULTISELECT,
+                options$: this.personTypesList$,
+                sortable: true
+            }),
+            new FilterModel({
+                fieldName: 'firstName',
+                fieldLabel: 'LNG_ENTITY_FIELD_LABEL_FIRST_NAME',
+                type: FilterType.TEXT,
+                sortable: true
+            }),
+            new FilterModel({
+                fieldName: 'lastName',
+                fieldLabel: 'LNG_ENTITY_FIELD_LABEL_LAST_NAME',
+                type: FilterType.TEXT,
+                sortable: true
+            }),
+            new FilterModel({
+                fieldName: 'visualId',
+                fieldLabel: 'LNG_ENTITY_FIELD_LABEL_VISUAL_ID',
+                type: FilterType.TEXT
+            }),
+            new FilterModel({
+                fieldName: 'gender',
+                fieldLabel: 'LNG_ENTITY_FIELD_LABEL_GENDER',
+                type: FilterType.MULTISELECT,
+                options$: this.genderList$,
+                sortable: true
+            }),
+            new FilterModel({
+                fieldName: 'age',
+                fieldLabel: 'LNG_ENTITY_FIELD_LABEL_AGE',
+                type: FilterType.RANGE_AGE,
+                sortable: true
+            }),
+            new FilterModel({
+                fieldName: 'addresses',
+                fieldLabel: 'LNG_ENTITY_FIELD_LABEL_ADDRESS',
+                type: FilterType.ADDRESS
+            }),
+            new FilterModel({
+                fieldName: 'dob',
+                fieldLabel: 'LNG_ENTITY_FIELD_LABEL_DOB',
+                type: FilterType.RANGE_DATE,
+                sortable: true
+            }),
+            new FilterModel({
+                fieldName: 'riskLevel',
+                fieldLabel: 'LNG_ENTITY_FIELD_LABEL_RISK',
+                type: FilterType.MULTISELECT,
+                options$: this.riskLevelsList$,
+                sortable: true
+            }),
+
+        ];
     }
 
 }
