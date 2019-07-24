7aca38f00aa88f78a016b2ee7229b77191f46e7b
Adrian Morariu <adrian.morariu@clarisoft.com>
2019-07-24 11:12:52 +0300

    WGD-2426 Discrepancy between data and epi curve

--- src/app/features/dashboard/components/epi-curve-dashlet/epi-curve-dashlet.component.ts
+++ src/app/features/dashboard/components/epi-curve-dashlet/epi-curve-dashlet.component.ts
@@ -211,7 +211,12 @@ export class EpiCurveDashletComponent implements OnInit, OnDestroy {
             if (this.globalFilterDate) {
                 qb.filter.byEquality(
                     'endDate',
-                    this.globalFilterDate.format('YYYY-MM-DD')
+                    this.globalFilterDate.clone().endOf('day').toISOString()
+                );
+            } else {
+                qb.filter.byEquality(
+                    'endDate',
+                    moment().endOf('day').toISOString()
                 );
             }
 
