7ac35fbe549e97a67e88adb8e3c7ff2525a64ffb
Adrian Morariu <adrian.morariu@clarisoft.com>
2019-08-07 13:12:10 +0300

    WGD-2447 Issue with how Locations are displayed on filters and entities

--- src/app/i18n/english_us.ts
+++ src/app/i18n/english_us.ts
@@ -12,49 +12,7 @@ export const EnglishUsLang = {
          */
         // 'LNG_EXAMPLE_TOKEN': 'Example value'
 
-        /**
-         * uiContactFields
-         */
-        'LNG_CONTACT_FIELD_LABEL_FOLLOW_UP_END_DATE': 'End of the followup',
-        'LNG_CONTACT_FIELD_LABEL_FOLLOW_UP_END_DATE_DESCRIPTION': '',
 
-        /**
-         * uiDashboardPage
-         */
-        'LNG_PAGE_DASHBOARD_KPI_CONTACTS_FOLLOWUP_LIST_TITLE': 'Contacts on the follow-up list ( {{date}} )',
-        'LNG_PAGE_DASHBOARD_KPI_CONTACTS_SEEN_EACH_DAY': 'Contacts seen ( {{date}} )',
-
-        /**
-         * uiChainsOfTransmissionBars
-         */
-        'LNG_PAGE_TRANSMISSION_CHAIN_BARS_CENTER_NAME_LABEL': 'Center name: {{name}}',
-        'LNG_PAGE_TRANSMISSION_CHAIN_BARS_CELL_WIDTH_LABEL': 'Cell width',
-        'LNG_PAGE_TRANSMISSION_CHAIN_BARS_LAB_PENDING_LABEL': '{{result}} (p)',
-        'LNG_PAGE_TRANSMISSION_CHAIN_BARS_LEGEND_DETAILS': '{{rectCD}} Case details: hs - Hospitalized / is - Isolated  <br/>{{rectLAB}} Lab result details: L? - unknown result / L- - negative result / L+ - positive result / (p) - waiting for result<br />{{rectOUT}} Outcome: oa - Alive / od - Deceased / or - Recovered<br />{{rectB}} Burial: bn - Buried no safe / bs - Buried safe',
-        'LNG_PAGE_TRANSMISSION_CHAIN_BARS_ISOLATED_CASE_LABEL': 'is',
-        'LNG_PAGE_TRANSMISSION_CHAIN_BARS_HOSPITALISED_CASE_LABEL': 'hs',
-        'LNG_PAGE_TRANSMISSION_CHAIN_BARS_LAB_RESULT_POSITIVE_LABEL': 'L+',
-        'LNG_PAGE_TRANSMISSION_CHAIN_BARS_LAB_RESULT_NEGATIVE_LABEL': 'L-',
-        'LNG_PAGE_TRANSMISSION_CHAIN_BARS_LAB_RESULT_UNKNOWN_LABEL': 'L?',
-        'LNG_PAGE_TRANSMISSION_CHAIN_BARS_HOSPITALISED_FOR_OTHER_CONDITIONS_CASE_LABEL': 'hs',
-        'LNG_PAGE_TRANSMISSION_CHAIN_BARS_OUTCOME_OTHER_LABEL': 'o?',
-        'LNG_PAGE_TRANSMISSION_CHAIN_BARS_OUTCOME_ALIVE_LABEL': 'oa',
-        'LNG_PAGE_TRANSMISSION_CHAIN_BARS_OUTCOME_DECEASED_LABEL': 'od',
-        'LNG_PAGE_TRANSMISSION_CHAIN_BARS_OUTCOME_RECOVERED_LABEL': 'or',
-        'LNG_PAGE_TRANSMISSION_CHAIN_BARS_OUTCOME_BURIAL_DATE_NOT_SAFE_LABEL': 'bn',
-        'LNG_PAGE_TRANSMISSION_CHAIN_BARS_OUTCOME_BURIAL_DATE_SAFE_LABEL': 'bs',
-        'LNG_PAGE_TRANSMISSION_CHAIN_BARS_FILTERS_CASE_CLASSIFICATION': 'Case classification',
-        'LNG_PAGE_TRANSMISSION_CHAIN_BARS_FILTERS_CASE_OUTCOME': 'Case outcome',
-
-        /**
-         * uiTeamsWorkload
-         */
-        'LNG_PAGE_TEAMS_WORKLOAD_TABLE_OF_LABEL': 'of',
-
-        /**
-         * uiGlobalFilterFields
-         */
-        'LNG_GLOBAL_FILTERS_FIELD_LABEL_CLASSIFICATION': 'Classification'
 
         /**
          * REMOVE the tokens from below
