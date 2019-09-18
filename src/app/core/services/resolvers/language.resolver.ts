8dab1635129b475874d16b25755c3b9c7c3624f3
Adrian Morariu <adrian.morariu@clarisoft.com>
2019-09-18 16:20:13 +0300

    WGD-2547 Remove hub / consolidation server logic

--- src/app/core/services/resolvers/language.resolver.ts
+++ src/app/core/services/resolvers/language.resolver.ts
@@ -4,6 +4,8 @@ import { TranslateService } from '@ngx-translate/core';
 import { I18nService } from '../helper/i18n.service';
 import { Observable, Subscriber } from 'rxjs';
 import { DialogService } from '../helper/dialog.service';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs/internal/observable/throwError';
 
 @Injectable()
 export class LanguageResolver implements Resolve<any> {
@@ -27,14 +29,27 @@ export class LanguageResolver implements Resolve<any> {
                 const loadingDialog = this.dialogService.showLoadingDialog();
 
                 // load language
-                this.i18nService.waitForLanguageInitialization().subscribe(() => {
-                    // hide loading
-                    loadingDialog.close();
+                this.i18nService.waitForLanguageInitialization()
+                    .pipe(
+                        catchError((err) => {
+                            // hide loading
+                            loadingDialog.close();
 
-                    // finished
-                    observer.next();
-                    observer.complete();
-                });
+                            // display error message
+                            alert('Error retrieving languages ( api might be down )');
+
+                            // finished
+                            return throwError(err);
+                        }),
+                    )
+                    .subscribe(() => {
+                        // hide loading
+                        loadingDialog.close();
+
+                        // finished
+                        observer.next();
+                        observer.complete();
+                    });
             }
         });
     }
