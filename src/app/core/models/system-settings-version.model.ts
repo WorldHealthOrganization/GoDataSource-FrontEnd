38b61957d1eb1d7114a2dbf3d2e7912c0ac8adc7
Adrian Morariu <adrian.morariu@clarisoft.com>
2019-09-18 14:30:39 +0300

    WGD-2547 Remove hub / consolidation server logic

--- src/app/core/models/system-settings-version.model.ts
+++ src/app/core/models/system-settings-version.model.ts
@@ -2,14 +2,12 @@ import * as _ from 'lodash';
 
 export class SystemSettingsVersionModel {
     platform: string;
-    type: string;
     version: string;
     build: string;
     arch: string;
 
     constructor(data = null) {
         this.platform = _.get(data, 'platform');
-        this.type = _.get(data, 'type');
         this.version = _.get(data, 'version');
         this.build = _.get(data, 'build');
         this.arch = _.get(data, 'arch');
