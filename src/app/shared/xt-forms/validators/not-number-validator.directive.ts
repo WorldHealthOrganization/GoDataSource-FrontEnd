df069c290c3dce6323c47fb3e6f41f9a8befd461
Adrian Morariu <adrian.morariu@clarisoft.com>
2019-10-02 17:02:39 +0300

    WGD-2332 Case export file does not include values for multiple questions

--- src/app/shared/xt-forms/validators/not-number-validator.directive.ts
+++ src/app/shared/xt-forms/validators/not-number-validator.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, forwardRef } from '@angular/core';
+import { Directive, forwardRef, Input } from '@angular/core';
 import { Validator, AbstractControl, NG_VALIDATORS } from '@angular/forms';
 import * as _ from 'lodash';
 
@@ -17,17 +17,21 @@ import * as _ from 'lodash';
  * Check if a form field is a valid e-mail address
  */
 export class NotNumberValidatorDirective implements Validator {
+    @Input() notNumberValidatorDisabled: boolean = false;
+
     validate(control: AbstractControl): { [key: string]: any } {
         if (
-            _.isEmpty(control.value) &&
-            !_.isNumber(control.value)
+            this.notNumberValidatorDisabled || (
+                _.isEmpty(control.value) &&
+                !_.isNumber(control.value)
+            )
         ) {
             return null;
         }
 
         const isValid: boolean = !_.isNumber(control.value) && (
             !_.isString(control.value) ||
-            !/[0-9.]+/.test(control.value)
+            !/^[0-9.]+$/.test(control.value)
         );
         return isValid ?
             null : {
