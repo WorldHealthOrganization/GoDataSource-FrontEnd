8c3981e91317ac2cc89d8074a074864a6f150518
Andrei.Frincu <andrei.frincu@clarisoft.com>
2019-10-15 12:53:33 +0300

    WGD-2572 Clicking reset password button blocks the ui

--- src/app/features/authentication/pages/forgot-password/forgot-password.component.ts
+++ src/app/features/authentication/pages/forgot-password/forgot-password.component.ts
@@ -56,6 +56,10 @@ export class ForgotPasswordComponent implements OnInit {
                 .pipe(
                     catchError((err) => {
                         this.snackbarService.showError(err.message);
+
+                        // hide dialog
+                        loadingDialog.close();
+
                         return throwError(err);
                     })
                 )
