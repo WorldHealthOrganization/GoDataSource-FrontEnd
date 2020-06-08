f9c3a19c92a1b7df35851b8bf6bb0cdeb44fa9bb
Adrian Morariu <adrian.morariu@clarisoft.com>
2020-06-08 15:57:09 +0300

    WGD-3111 Case import issue

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,7 @@ export const environment = {
     // enable Production mode
     production: false,
     // API base URL
-    // apiUrl: 'http://whoapicd.clarisoft.com/api',
-    apiUrl: 'http://localhost:3000/api',
+    apiUrl: 'http://whoapicd.clarisoft.com/api',
     // log messages on server?
     enableApiLogging: false,
     // log messages in browser's console?
