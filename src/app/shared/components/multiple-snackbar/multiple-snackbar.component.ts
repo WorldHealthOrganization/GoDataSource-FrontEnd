56ff4665e847b81157fb3020459b099e77cc8a76
andrei-frincu <user#0704>
2020-07-08 18:03:31 +0300

    WGD-2649 Error and confirmation messages should remain on screen

--- src/app/shared/components/multiple-snackbar/multiple-snackbar.component.ts
+++ src/app/shared/components/multiple-snackbar/multiple-snackbar.component.ts
@@ -1,8 +1,8 @@
 import { Component, Inject, OnDestroy, ViewEncapsulation } from '@angular/core';
 import { MAT_SNACK_BAR_DATA, MatSnackBarRef } from '@angular/material';
-import * as _ from 'lodash';
 import { DebounceTimeCaller } from '../../../core/helperClasses/debounce-time-caller';
 import { Subscriber } from 'rxjs';
+import { v4 as uuid } from 'uuid';
 
 @Component({
     selector: 'app-multiple-snackbar',
@@ -10,18 +10,17 @@ import { Subscriber } from 'rxjs';
     templateUrl: './multiple-snackbar.component.html',
     styleUrls: ['./multiple-snackbar.component.less']
 })
-export class MultipleSnackbarComponent {
+export class MultipleSnackbarComponent implements OnDestroy {
 
     // snackbar messages
     messages: {
+        id?: string,
         message: string,
         messageClass: string,
         html: boolean,
-        duration: number
+        duration: number,
+        messageDurationHandler: DebounceTimeCaller
     }[] = [];
-
-    // available themes: 'success', 'error'
-    theme: string;
 
     /**
      * Constructor
@@ -29,41 +28,61 @@ export class MultipleSnackbarComponent {
     constructor(
         @Inject(MAT_SNACK_BAR_DATA) public data: any,
         public snackBarRef: MatSnackBarRef<MultipleSnackbarComponent>
-    ) {
-        this.theme = _.get(data, 'theme');
-    }
+    ) {}
 
     /**
      * Add message
      */
     addMessage(message) {
-        // add message to be displayed
-        this.messages.push(message);
-        // if message have duration time, trigger close function based on message duration
-        if (message.duration) {
-            // message index
-            const messageIndex = _.findIndex(this.messages, message);
-            // create trigger to close message for messages that have a duration do display them
-            const triggerCloseMessage = new DebounceTimeCaller(new Subscriber<void>(() => {
-                this.closeSnackbar(messageIndex);
-            }));
-            // call the closeTrigger
-            triggerCloseMessage.callAfterMs(message.duration);
+        // if we've already pushed this static message, do not push again the message
+        if (
+            message.id &&
+            this.messages.find(msg => {
+                return msg.id === message.id;
+            })
+        ) {
+            return;
+        } else {
+            // assign unique id to message if it doesn't have one
+            if (!message.id) {
+                Object.assign(message, {id: uuid()});
+            }
+            // add message to be displayed
+            this.messages.push(message);
+            // if message have duration time, trigger close function based on message duration
+            if (message.duration) {
+                // create trigger to close message for messages that have a duration do display them
+                message.messageDurationHandler = new DebounceTimeCaller(new Subscriber<void>(() => {
+                    this.closeSnackbar(message.id);
+                }));
+                // call the closeTrigger
+                message.messageDurationHandler.callAfterMs(message.duration);
+            }
         }
     }
 
     /**
      * Close snackbar
      */
-    closeSnackbar(ind) {
-        // reset messages collection and dismiss snackbar if there is only one message
-        if (this.messages.length === 1) {
-            this.closeAllSnackbars();
-        } else {
-            // close message
-            this.messages = this.messages.filter((msg, index) => {
-                return index !== ind ? msg : '';
+    closeSnackbar(id) {
+        // find message index
+        const index: number = this.messages.findIndex(msg => msg.id === id);
+        if (index > -1) {
+            // if message has duration unsubscribe debounce time caller
+            if (this.messages[index].messageDurationHandler) {
+                this.messages[index].messageDurationHandler.unsubscribe();
+                this.messages[index].messageDurationHandler = undefined;
+            }
+
+            // remove specific message
+            this.messages = this.messages.filter((msg) => {
+                return msg.id !== id;
             });
+        }
+
+        // dismiss all snackbars
+        if (this.messages.length < 1) {
+            this.closeAllSnackbars();
         }
     }
 
@@ -74,5 +93,18 @@ export class MultipleSnackbarComponent {
         this.messages = [];
         this.data.dismissSnackbar();
         this.snackBarRef.dismiss();
+    }
+
+    /**
+     * Component destroyed
+     */
+    ngOnDestroy(): void {
+        // unsubscribe for each message if it has a duration handled
+        this.messages.forEach((message) => {
+            if (message.messageDurationHandler) {
+                message.messageDurationHandler.unsubscribe();
+                message.messageDurationHandler = undefined;
+            }
+        });
     }
 }
