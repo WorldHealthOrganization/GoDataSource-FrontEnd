6a508218689c3ca6865ffcb74582f0e9a81ef2b4
Adrian Morariu <adrian.morariu@clarisoft.com>
2020-07-09 17:46:22 +0300

    WGD-3251 Reduce COT data retrieved from API by retrieving only the necessary fields

--- src/app/core/services/data/outbreak.data.service.ts
+++ src/app/core/services/data/outbreak.data.service.ts
@@ -352,6 +352,10 @@ export class OutbreakDataService {
                         AppMessages.APP_MESSAGE_UNRESPONSIVE_NO_ACTIVE_OUTBREAK
                     );
                 } else {
+                    // hide message
+                    this.snackbarService.hideMessage(AppMessages.APP_MESSAGE_UNRESPONSIVE_NO_ACTIVE_OUTBREAK);
+
+                    // outbreak not active ?
                     if (authUser.activeOutbreakId !== selectedOutbreak.id) {
                         this.getOutbreak(authUser.activeOutbreakId)
                             .subscribe((outbreak) => {
@@ -365,6 +369,9 @@ export class OutbreakDataService {
                                     AppMessages.APP_MESSAGE_UNRESPONSIVE_SELECTED_OUTBREAK_NOT_ACTIVE
                                 );
                             });
+                    } else {
+                        // hide message
+                        this.snackbarService.hideMessage(AppMessages.APP_MESSAGE_UNRESPONSIVE_SELECTED_OUTBREAK_NOT_ACTIVE);
                     }
                 }
             });
