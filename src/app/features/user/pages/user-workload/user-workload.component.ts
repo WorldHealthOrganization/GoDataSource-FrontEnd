ae9bcb0cac9c218e03156f82eedc15503299a804
Adrian Morariu <adrian.morariu@clarisoft.com>
2021-08-10 10:32:38 +0300

    Some minor fixes

--- src/app/features/user/pages/user-workload/user-workload.component.ts
+++ src/app/features/user/pages/user-workload/user-workload.component.ts
@@ -125,6 +125,9 @@ export class UserWorkloadComponent extends ListComponent implements OnInit, OnDe
                                 low: moment().add(-this.selectedOutbreak.periodOfFollowup + 1, 'days').startOf('day'),
                                 high: moment().add(1, 'days').endOf('day')
                             });
+                        } else {
+                            // hide loading
+                            this.displayLoading = false;
                         }
                     });
             });
@@ -197,6 +200,9 @@ export class UserWorkloadComponent extends ListComponent implements OnInit, OnDe
                     .getFollowUpsPerDayUser(this.selectedOutbreak.id, this.queryBuilder)
                     .pipe(
                         catchError((err) => {
+                            // hide loading
+                            this.displayLoading = false;
+
                             this.snackbarService.showApiError(err);
                             finishCallback([]);
                             return throwError(err);
@@ -220,6 +226,10 @@ export class UserWorkloadComponent extends ListComponent implements OnInit, OnDe
                 finishCallback([]);
             }
         } else {
+            // hide loading
+            this.displayLoading = false;
+
+            // finished
             finishCallback([]);
         }
     }
