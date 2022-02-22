29bbda3101c97ef746821da6d110612110e66ca1
Adrian Morariu <adrian.morariu@clarisoft.com>
2022-02-22 15:45:38 +0200

    WGD-3567 PRIORITY: UI/UX Improvement

--- src/app/core/components/index.ts
+++ src/app/core/components/index.ts
@@ -2,12 +2,10 @@
 import { AuthenticatedComponent } from './authenticated/authenticated.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { RedirectComponent } from './redirect/redirect.component';
-import { AppFormDebugComponent } from './form-debug/app-form-debug.component';
 
 // export the list of all components
 export const components: any[] = [
   AuthenticatedComponent,
   SidenavComponent,
-  RedirectComponent,
-  AppFormDebugComponent
+  RedirectComponent
 ];
