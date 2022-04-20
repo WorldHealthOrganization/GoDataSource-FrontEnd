539b2f4ee8950af3abdb84f1704c843bcff466b1
Adrian Morariu <adrian.morariu@clarisoft.com>
2022-04-20 13:17:51 +0300

    WGD-3567 PRIORITY: UI/UX Improvement

--- src/app/shared/components-v2/app-create-view-modify-v2/app-create-view-modify-v2.component.ts
+++ src/app/shared/components-v2/app-create-view-modify-v2/app-create-view-modify-v2.component.ts
@@ -150,6 +150,11 @@ export class AppCreateViewModifyV2Component {
 
       // re-render ui
       this.changeDetectorRef.detectChanges();
+
+      // needed to update mat tab label warnings
+      if (this.isModify) {
+        this.changeDetectorRef.markForCheck();
+      }
     };
 
     // ask for confirmation
