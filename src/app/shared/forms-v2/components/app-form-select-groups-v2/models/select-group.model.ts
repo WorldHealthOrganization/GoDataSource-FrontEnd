b5724321a53f3fa31474f857fe9c77bb93fcbd63
Adrian Morariu <adrian.morariu@clarisoft.com>
2022-04-30 11:57:24 +0300

    WGD-3567 PRIORITY: UI/UX Improvement

---
+++ src/app/shared/forms-v2/components/app-form-select-groups-v2/models/select-group.model.ts
@@ -0,0 +1,64 @@
+import { SafeHtml } from '@angular/platform-browser';
+
+/**
+ * Used by events
+ */
+export enum GroupEventDataAction {
+  None = 'none',
+  Partial = 'partial',
+  All = 'all'
+}
+
+/**
+ * Used for event input data
+ */
+export interface IGroupEventData {
+  readonly group: any;
+  readonly action: GroupEventDataAction;
+
+  value: string[];
+  readonly groupsMap: ISelectGroupMap<any>;
+  readonly optionsMap: ISelectGroupOptionMap<any>;
+  readonly previousValue: string[];
+  addValues(...values: string[]): string[];
+}
+
+/**
+ * Used for event input data
+ */
+export interface IGroupOptionEventData {
+  readonly group: any;
+  readonly option: any;
+  readonly checked: boolean;
+
+  value: string[];
+  readonly groupsMap: ISelectGroupMap<any>;
+  readonly optionsMap: ISelectGroupOptionMap<any>;
+  readonly allWasSelected: boolean;
+  addValues(...values: string[]): string[];
+}
+
+/**
+ * Used to map groups for easy access to a group by using its unique key
+ */
+export interface ISelectGroupMap<T> {
+  [groupValueKey: string]: T;
+}
+
+/**
+ * Used to map group child options for easy access to a options and its parent group by using its unique key
+ */
+export interface ISelectGroupOptionMap<T> {
+  [optionValue: string]: {
+    groupValue: string,
+    option: T
+  };
+}
+
+/**
+ * Used to format child option labels & tooltips
+ */
+export interface ISelectGroupOptionFormatResponse {
+  label: SafeHtml;
+  tooltip: string;
+}
