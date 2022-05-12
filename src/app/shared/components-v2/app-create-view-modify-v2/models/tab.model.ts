882e8a613b8d2bad237b01d75379e102ba6fe9d3
Adrian Morariu <adrian.morariu@clarisoft.com>
2022-05-12 15:38:40 +0300

    WGD-3567 PRIORITY: UI/UX Improvement

--- src/app/shared/components-v2/app-create-view-modify-v2/models/tab.model.ts
+++ src/app/shared/components-v2/app-create-view-modify-v2/models/tab.model.ts
@@ -14,7 +14,6 @@ import { UserSettings } from '../../../../core/models/user.model';
 import { IBasicCount } from '../../../../core/models/basic-count.interface';
 import { RequestQueryBuilder } from '../../../../core/helperClasses/request-query-builder';
 import { V2AdvancedFilter } from '../../app-list-table-v2/models/advanced-filter.model';
-import { IV2ProcessSelectedData } from '../../app-list-table-v2/models/process-data.model';
 
 /**
  * Input type
@@ -371,7 +370,6 @@ export interface ICreateViewModifyV2TabTable {
   // used by ui
   updateUI?: () => void;
   records$?: Observable<any[]>;
-  processSelectedData?: IV2ProcessSelectedData[];
   queryBuilder?: RequestQueryBuilder;
   applyHasMoreLimit?: boolean;
   pageCount?: IBasicCount;
