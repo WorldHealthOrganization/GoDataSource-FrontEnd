f16dd45d22a55c70f4aa5fca59a4afd4521042a7
Adrian Morariu <adrian.morariu@clarisoft.com>
2022-05-13 14:54:26 +0300

    WGD-3567 PRIORITY: UI/UX Improvement

--- src/app/features/outbreak/pages/search-result-list/search-result-list.component.ts
+++ src/app/features/outbreak/pages/search-result-list/search-result-list.component.ts
@@ -16,10 +16,15 @@ import { GlobalEntitySearchDataService } from '../../../../core/services/data/gl
 import { catchError, takeUntil } from 'rxjs/operators';
 import { ToastV2Service } from '../../../../core/services/helper/toast-v2.service';
 import * as _ from 'lodash';
-import { IV2ColumnStatusFormType, V2ColumnFormat, V2ColumnStatusForm } from '../../../../shared/components-v2/app-list-table-v2/models/column.model';
+import { IV2ColumnPinned, IV2ColumnStatusFormType, V2ColumnFormat, V2ColumnStatusForm } from '../../../../shared/components-v2/app-list-table-v2/models/column.model';
 import { IResolverV2ResponseModel } from '../../../../core/services/resolvers/data/models/resolver-response.model';
 import { ReferenceDataEntryModel } from '../../../../core/models/reference-data.model';
 import { TranslateService } from '@ngx-translate/core';
+import { RelationshipModel } from '../../../../core/models/entity-and-relationship.model';
+import { EntityHelperService } from '../../../../core/services/helper/entity-helper.service';
+import { V2ActionType } from '../../../../shared/components-v2/app-list-table-v2/models/action.model';
+import { EntityType } from '../../../../core/models/entity-type';
+import { DashboardModel } from '../../../../core/models/dashboard.model';
 
 @Component({
   selector: 'app-search-result-list',
@@ -42,7 +47,8 @@ export class SearchResultListComponent extends ListComponent implements OnDestro
     private globalEntitySearchDataService: GlobalEntitySearchDataService,
     private toastV2Service: ToastV2Service,
     private activatedRoute: ActivatedRoute,
-    private translateService: TranslateService
+    private translateService: TranslateService,
+    private entityHelperService: EntityHelperService
   ) {
     // parent
     super(listHelperService);
@@ -137,6 +143,93 @@ export class SearchResultListComponent extends ListComponent implements OnDestro
         field: 'name',
         label: 'LNG_ENTITY_FIELD_LABEL_NAME',
         highlight: this._searchValue
+      },
+      {
+        field: 'numberOfContacts',
+        label: 'LNG_ENTITY_FIELD_LABEL_NUMBER_OF_CONTACTS',
+        format: {
+          type: V2ColumnFormat.BUTTON
+        },
+        cssCellClass: 'gd-cell-button',
+        buttonLabel: (item) =>
+          (item.numberOfContacts || '').toLocaleString('en'),
+        color: 'text',
+        click: (item) => {
+          // if we do not have contacts return
+          if (item.numberOfContacts < 1) {
+            return;
+          }
+
+          // display dialog
+          this.entityHelperService.contacts(this.selectedOutbreak, item);
+        },
+        disabled: (data) => !RelationshipModel.canList(this.authUser) || !data.canListRelationshipContacts(this.authUser)
+      },
+      {
+        field: 'numberOfExposures',
+        label: 'LNG_ENTITY_FIELD_LABEL_NUMBER_OF_EXPOSURES',
+        format: {
+          type: V2ColumnFormat.BUTTON
+        },
+        cssCellClass: 'gd-cell-button',
+        buttonLabel: (item) =>
+          (item.numberOfExposures || '').toLocaleString('en'),
+        color: 'text',
+        click: (item) => {
+          // if we do not have exposures return
+          if (item.numberOfExposures < 1) {
+            return;
+          }
+
+          // display dialog
+          this.entityHelperService.exposures(this.selectedOutbreak, item);
+        },
+        disabled: (data) => !RelationshipModel.canList(this.authUser) || !data.canListRelationshipExposures(this.authUser)
+      },
+      // actions
+      {
+        field: 'actions',
+        label: 'LNG_COMMON_LABEL_ACTIONS',
+        pinned: IV2ColumnPinned.RIGHT,
+        notResizable: true,
+        cssCellClass: 'gd-cell-no-focus',
+        format: {
+          type: V2ColumnFormat.ACTIONS
+        },
+        actions: [
+          // View Event
+          {
+            type: V2ActionType.ICON,
+            icon: 'visibility',
+            iconTooltip: 'LNG_PAGE_ACTION_VIEW',
+            action: {
+              link: (item: CaseModel | ContactModel | ContactOfContactModel | EventModel): string[] => {
+                return [this.getItemRouterLink(item, 'view')];
+              }
+            },
+            visible: (item: CaseModel | ContactModel | ContactOfContactModel | EventModel): boolean => {
+              return !item.deleted &&
+                item.canView(this.authUser);
+            }
+          },
+
+          // Modify Event
+          {
+            type: V2ActionType.ICON,
+            icon: 'edit',
+            iconTooltip: 'LNG_PAGE_ACTION_MODIFY',
+            action: {
+              link: (item: CaseModel | ContactModel | ContactOfContactModel | EventModel): string[] => {
+                return [this.getItemRouterLink(item, 'modify')];
+              }
+            },
+            visible: (item: CaseModel | ContactModel | ContactOfContactModel | EventModel): boolean => {
+              return !item.deleted &&
+                this.selectedOutbreakIsActive &&
+                item.canModify(this.authUser);
+            }
+          }
+        ]
       }
     ];
   }
@@ -149,7 +242,11 @@ export class SearchResultListComponent extends ListComponent implements OnDestro
   /**
    * Initialize table infos
    */
-  protected initializeTableInfos(): void {}
+  protected initializeTableInfos(): void {
+    this.infos = [
+      'LNG_PAGE_LIST_SEARCH_RESULT_DESCRIPTION'
+    ];
+  }
 
   /**
    * Initialize Table Advanced Filters
@@ -179,13 +276,37 @@ export class SearchResultListComponent extends ListComponent implements OnDestro
   /**
    * Initialize breadcrumbs
    */
-  protected initializeBreadcrumbs(): void {}
+  protected initializeBreadcrumbs(): void {
+    // set breadcrumbs
+    this.breadcrumbs = [
+      {
+        label: 'LNG_COMMON_LABEL_HOME',
+        action: {
+          link: DashboardModel.canViewDashboard(this.authUser) ?
+            ['/dashboard'] :
+            ['/account/my-profile']
+        }
+      }, {
+        label: 'LNG_PAGE_LIST_SEARCH_RESULT_TITLE',
+        action: null
+      }
+    ];
+  }
 
   /**
    * Fields retrieved from api to reduce payload size
    */
   protected refreshListFields(): string[] {
-    return [];
+    return [
+      'id',
+      'visualId',
+      'name',
+      'firstName',
+      'lastName',
+      'type',
+      'numberOfContacts',
+      'numberOfExposures'
+    ];
   }
 
   /**
@@ -247,261 +368,22 @@ export class SearchResultListComponent extends ListComponent implements OnDestro
       });
   }
 
-  // Breadcrumbs
-  // breadcrumbs: BreadcrumbItemModel[] = [
-  //   new BreadcrumbItemModel('LNG_PAGE_LIST_SEARCH_RESULT_TITLE', '.', true)
-  // ];
+  /**
+   * Get the link to redirect to view page depending on item type and action
+   */
+  private getItemRouterLink(item: CaseModel | ContactModel | ContactOfContactModel | EventModel, action: string): string {
+    switch (item.type) {
+      case EntityType.CASE:
+        return `/cases/${item.id}/${action === 'view' ? 'view' : 'modify'}`;
+      case EntityType.CONTACT:
+        return `/contacts/${item.id}/${action === 'view' ? 'view' : 'modify'}`;
+      case EntityType.CONTACT_OF_CONTACT:
+        return `/contacts-of-contacts/${item.id}/${action === 'view' ? 'view' : 'modify'}`;
+      case EntityType.EVENT:
+        return `/events/${item.id}/${action === 'view' ? 'view' : 'modify'}`;
+    }
 
-  // searchValue: string;
-  //
-  // // list of search result
-  // entityList$: Observable<(CaseModel | ContactModel | ContactOfContactModel | EventModel)[]>;
-  // entityListCount$: Observable<IBasicCount>;
-  //
-  // // models
-  // personTypesListMap: { [id: string]: ReferenceDataEntryModel };
-  //
-  // // constants
-  // EntityType = EntityType;
-  // UserSettings = UserSettings;
-  // ReferenceDataCategory = ReferenceDataCategory;
-  // OutbreakModel = OutbreakModel;
-  //
-  // // subscribers
-  // outbreakSubscriber: Subscription;
-  //
-  // recordActions: HoverRowAction[] = [
-  //   // View Item
-  //   new HoverRowAction({
-  //     icon: 'visibility',
-  //     iconTooltip: 'LNG_PAGE_ACTION_VIEW',
-  //     linkGenerator: (item: CaseModel | ContactModel | ContactOfContactModel | EventModel): string[] => {
-  //       return [this.getItemRouterLink(item, 'view')];
-  //     },
-  //     visible: (item: CaseModel | ContactModel | ContactOfContactModel | EventModel): boolean => {
-  //       return !item.deleted &&
-  //                   this.authUser &&
-  //                   this.canViewItem(item);
-  //     }
-  //   }),
-  //
-  //   // Modify Item
-  //   new HoverRowAction({
-  //     icon: 'settings',
-  //     iconTooltip: 'LNG_PAGE_ACTION_MODIFY',
-  //     linkGenerator: (item: CaseModel | ContactModel | ContactOfContactModel | EventModel): string[] => {
-  //       return [this.getItemRouterLink(item, 'modify')];
-  //     },
-  //     visible: (item: CaseModel | ContactModel | ContactOfContactModel | EventModel): boolean => {
-  //       return !item.deleted &&
-  //                   this.authUser &&
-  //                   this.canModifyItem(item);
-  //     }
-  //   })
-  // ];
-  //
-  // /**
-  //    * Constructor
-  //    */
-  // constructor(
-  //   protected listHelperService: ListHelperService,
-  //   private globalEntitySearchDataService: GlobalEntitySearchDataService,
-  //   private outbreakDataService: OutbreakDataService,
-  //   private toastV2Service: ToastV2Service,
-  //   private referenceDataDataService: ReferenceDataDataService,
-  //   private route: ActivatedRoute
-  // ) {
-  //   super(
-  //     listHelperService,
-  //     true
-  //   );
-  // }
-  //
-  // /**
-  //    * Component initialized
-  //    */
-  // ngOnInit() {
-  //   // reference data
-  //   const personTypes$ = this.referenceDataDataService.getReferenceDataByCategory(ReferenceDataCategory.PERSON_TYPE).pipe(share());
-  //   personTypes$.subscribe((personTypeCategory: ReferenceDataCategoryModel) => {
-  //     this.personTypesListMap = _.transform(
-  //       personTypeCategory.entries,
-  //       (result, entry: ReferenceDataEntryModel) => {
-  //         // groupBy won't work here since groupBy will put an array instead of one value
-  //         result[entry.id] = entry;
-  //       },
-  //       {}
-  //     );
-  //   });
-  //
-  //   // subscribe to the Selected Outbreak Subject stream
-  //   this.outbreakSubscriber = this.outbreakDataService
-  //     .getSelectedOutbreakSubject()
-  //     .subscribe((selectedOutbreak: OutbreakModel) => {
-  //       this.selectedOutbreak = selectedOutbreak;
-  //
-  //       // initialize pagination
-  //       this.initPaginator();
-  //
-  //       // retrieve query params
-  //       this.route.queryParams
-  //         .subscribe((params: { searchValue }) => {
-  //           this.searchValue = _.get(params, 'search');
-  //
-  //           // ...and re-load the list when the Selected Outbreak is changed
-  //           this.needsRefreshList(true);
-  //         });
-  //     });
-  //
-  //   // initialize Side Table Columns
-  //   this.initializeTableColumns();
-  // }
-  //
-  // /**
-  //    * Release resources
-  //    */
-  // ngOnDestroy() {
-  //   // release parent resources
-  //   super.onDestroy();
-  // }
-  //
-  // /**
-  //    * Initialize Side Table Columns
-  //    */
-  // initializeTableColumns() {
-  //   // default table columns
-  //   // this.tableColumns = [
-  //   //   new VisibleColumnModel({
-  //   //     field: 'id',
-  //   //     label: 'LNG_ENTITY_FIELD_LABEL_ID'
-  //   //   }),
-  //   //   new VisibleColumnModel({
-  //   //     field: 'visualId',
-  //   //     label: 'LNG_ENTITY_FIELD_LABEL_VISUAL_ID'
-  //   //   }),
-  //   //   new VisibleColumnModel({
-  //   //     field: 'name',
-  //   //     label: 'LNG_ENTITY_FIELD_LABEL_NAME'
-  //   //   })
-  //   // ];
-  // }
-  //
-  // /**
-  //  * Initialize process data
-  //  */
-  // protected initializeProcessSelectedData(): void {}
-  //
-  // /**
-  //  * Initialize table infos
-  //  */
-  // protected initializeTableInfos(): void {}
-  //
-  // /**
-  //  * Initialize Table Advanced Filters
-  //  */
-  // protected initializeTableAdvancedFilters(): void {}
-  //
-  // /**
-  //  * Initialize table quick actions
-  //  */
-  // protected initializeQuickActions(): void {}
-  //
-  // /**
-  //  * Initialize table group actions
-  //  */
-  // protected initializeGroupActions(): void {}
-  //
-  // /**
-  //  * Initialize table add action
-  //  */
-  // protected initializeAddAction(): void {}
-  //
-  // /**
-  //  * Initialize table grouped data
-  //  */
-  // protected initializeGroupedData(): void {}
-  //
-  // /**
-  //  * Initialize breadcrumbs
-  //  */
-  // initializeBreadcrumbs(): void {
-  // }
-  //
-  // /**
-  //  * Fields retrieved from api to reduce payload size
-  //  */
-  // protected refreshListFields(): string[] {
-  //   return [];
-  // }
-
-  // /**
-  //    * Retrieve Person Type color
-  //    */
-  // getPersonTypeColor(personType: string) {
-  //   const personTypeData = _.get(this.personTypesListMap, personType);
-  //   return _.get(personTypeData, 'colorCode', '');
-  // }
-  //
-  // /**
-  //    * Check if we can view item
-  //    * @param {Object} item
-  //    * @returns {boolean}
-  //    */
-  // canViewItem(item: CaseModel | ContactModel | ContactOfContactModel | EventModel): boolean {
-  //   // check if we can modify item
-  //   switch (item.type) {
-  //     case EntityType.CASE:
-  //       return CaseModel.canView(this.authUser);
-  //     case EntityType.CONTACT:
-  //       return ContactModel.canView(this.authUser);
-  //     case EntityType.CONTACT_OF_CONTACT:
-  //       return ContactOfContactModel.canModify(this.authUser);
-  //     case EntityType.EVENT:
-  //       return EventModel.canView(this.authUser);
-  //   }
-  //
-  //   return false;
-  // }
-  //
-  // /**
-  //    * Check if we can modify item
-  //    * @param {Object} item
-  //    * @returns {boolean}
-  //    */
-  // canModifyItem(item: CaseModel | ContactModel | ContactOfContactModel | EventModel): boolean {
-  //   // check if we can modify item
-  //   switch (item.type) {
-  //     case EntityType.CASE:
-  //       return CaseModel.canModify(this.authUser);
-  //     case EntityType.CONTACT:
-  //       return ContactModel.canModify(this.authUser);
-  //     case EntityType.CONTACT_OF_CONTACT:
-  //       return ContactOfContactModel.canModify(this.authUser);
-  //     case EntityType.EVENT:
-  //       return EventModel.canModify(this.authUser);
-  //   }
-  //
-  //   return false;
-  // }
-  //
-  // /**
-  //    * Get the link to redirect to view page depending on item type and action
-  //    * @param {Object} item
-  //    * @param {string} action
-  //    * @returns {string}
-  //    */
-  // getItemRouterLink(item: CaseModel | ContactModel | ContactOfContactModel | EventModel, action: string): string {
-  //   switch (item.type) {
-  //     case EntityType.CASE:
-  //       return `/cases/${item.id}/${action === 'view' ? 'view' : 'modify'}`;
-  //     case EntityType.CONTACT:
-  //       return `/contacts/${item.id}/${action === 'view' ? 'view' : 'modify'}`;
-  //     case EntityType.CONTACT_OF_CONTACT:
-  //       return `/contacts-of-contacts/${item.id}/${action === 'view' ? 'view' : 'modify'}`;
-  //     case EntityType.EVENT:
-  //       return `/events/${item.id}/${action === 'view' ? 'view' : 'modify'}`;
-  //   }
-  //
-  //   return '';
-  // }
+    // ...
+    return '';
+  }
 }
