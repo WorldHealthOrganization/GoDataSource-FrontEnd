0e65ca4039123fb89e23dfe556ba5d302872b159
Adrian Morariu <adrian.morariu@clarisoft.com>
2022-05-18 12:31:03 +0300

    WGD-3567 PRIORITY: UI/UX Improvement

--- src/app/core/services/resolvers/data/questionnaire-answer-display.resolver.ts
+++ src/app/core/services/resolvers/data/questionnaire-answer-display.resolver.ts
@@ -32,7 +32,7 @@ export class QuestionnaireAnswerDisplayDataResolver implements IMapResolverV2<IL
           };
           data.forEach((item) => {
             // map
-            response.map[item.id] = item;
+            response.map[item.value] = item;
 
             // add option
             response.options.push({
