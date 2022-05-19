8e229ea2d1efe6d54512d92d6d1765209f139e64
Adrian Morariu <adrian.morariu@clarisoft.com>
2022-05-19 16:16:23 +0300

    WGD-3567 PRIORITY: UI/UX Improvement

--- src/app/shared/forms-v2/components/app-form-fill-questionnaire-v2/app-form-fill-questionnaire-v2.component.ts
+++ src/app/shared/forms-v2/components/app-form-fill-questionnaire-v2/app-form-fill-questionnaire-v2.component.ts
@@ -7,6 +7,7 @@ import { determineRenderMode, RenderMode } from '../../../../core/enums/render-m
 import { Constants } from '../../../../core/models/constants';
 import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
 import { ILabelValuePairModel } from '../../core/label-value-pair.model';
+import * as moment from 'moment';
 
 /**
  * Flatten type
@@ -137,7 +138,10 @@ export class AppFormFillQuestionnaireV2Component
     this._questionnaire = questionnaire;
 
     // flatten
-    this.nonFlatToFlat(false);
+    this.nonFlatToFlat(
+      false,
+      true
+    );
   }
 
   // render mode
@@ -193,7 +197,10 @@ export class AppFormFillQuestionnaireV2Component
     super.writeValue(value);
 
     // flatten
-    this.nonFlatToFlat(false);
+    this.nonFlatToFlat(
+      false,
+      true
+    );
   }
 
   /**
@@ -206,7 +213,10 @@ export class AppFormFillQuestionnaireV2Component
   /**
    * Convert non flat value to flat value
    */
-  private nonFlatToFlat(waited: boolean): void {
+  private nonFlatToFlat(
+    waited: boolean,
+    checkForMultiAnswerMissingValues: boolean
+  ): void {
     // wait for everything to be bound
     if (
       !this.value ||
@@ -229,11 +239,21 @@ export class AppFormFillQuestionnaireV2Component
         this._nonFlatToFlatWait = undefined;
 
         // execute
-        this.nonFlatToFlat(true);
+        this.nonFlatToFlat(
+          true,
+          checkForMultiAnswerMissingValues
+        );
       });
 
       // finished
       return;
+    }
+
+    // make sure all multi answer questions have the proper responses
+    if (checkForMultiAnswerMissingValues) {
+      this._questionnaire.forEach((question) => {
+        this.fillOutValuesForMultiAnswerQuestion(question);
+      });
     }
 
     // flatten
@@ -242,7 +262,8 @@ export class AppFormFillQuestionnaireV2Component
       this._questionnaire,
       0,
       null,
-      false
+      false,
+      0
     );
   }
 
@@ -253,7 +274,8 @@ export class AppFormFillQuestionnaireV2Component
     questions: QuestionModel[],
     level: number,
     parent: IFlattenNodeAnswer,
-    oneParentIsInactive: boolean
+    oneParentIsInactive: boolean,
+    answerParentIndex: number
   ): void {
     // no questions ?
     if (
@@ -326,29 +348,33 @@ export class AppFormFillQuestionnaireV2Component
                 value: answer.value
               }));
 
-              // flatten
-              const flattenedAnswer: IFlattenNodeAnswer = {
-                type: FlattenType.ANSWER,
-                level: flattenedQuestion.level + 1,
-                parent: flattenedQuestion,
-                oneParentIsInactive: (flattenedQuestion.data as QuestionModel).inactive || flattenedQuestion.oneParentIsInactive,
-                name: `${this.name}[${question.variable}]`,
-                index: 0,
-                definition: question.answerType === Constants.ANSWER_TYPES.MULTIPLE_OPTIONS.value ?
-                  {
-                    type: FlattenAnswerDrawType.MULTIPLE_SELECT,
-                    options
-                  } : {
-                    type: FlattenAnswerDrawType.SINGLE_SELECT,
-                    options
-                  }
-              };
+              // render
+              const render = (answerIndex: number) => {
+                // get item
+                const item: IAnswerData = this.value[flattenedQuestion.data.variable][answerIndex];
 
-              // add to list
-              this.flattenedQuestions.push(flattenedAnswer);
+                // flatten
+                const flattenedAnswer: IFlattenNodeAnswer = {
+                  type: FlattenType.ANSWER,
+                  level: flattenedQuestion.level + 1,
+                  parent: flattenedQuestion,
+                  oneParentIsInactive: (flattenedQuestion.data as QuestionModel).inactive || flattenedQuestion.oneParentIsInactive,
+                  name: `${this.name}[${question.variable}][${answerIndex}].value`,
+                  index: answerIndex,
+                  definition: question.answerType === Constants.ANSWER_TYPES.MULTIPLE_OPTIONS.value ?
+                    {
+                      type: FlattenAnswerDrawType.MULTIPLE_SELECT,
+                      options
+                    } : {
+                      type: FlattenAnswerDrawType.SINGLE_SELECT,
+                      options
+                    }
+                };
 
-              // determine if we need to show other things depending on what was selected
-              this.value[flattenedQuestion.data.variable].forEach((item) => {
+                // add to list
+                this.flattenedQuestions.push(flattenedAnswer);
+
+                // determine if we need to show other things depending on what was selected
                 // multiple answer question ?
                 if (question.answerType === Constants.ANSWER_TYPES.MULTIPLE_OPTIONS.value) {
                   // go through each multiple response
@@ -360,7 +386,8 @@ export class AppFormFillQuestionnaireV2Component
                           answersWithQuestionsMap[answerValue].additionalQuestions,
                           flattenedAnswer.level + 1,
                           flattenedAnswer,
-                          flattenedAnswer.oneParentIsInactive
+                          flattenedAnswer.oneParentIsInactive,
+                          answerIndex
                         );
                       }
                     });
@@ -372,11 +399,21 @@ export class AppFormFillQuestionnaireV2Component
                       answersWithQuestionsMap[item.value].additionalQuestions,
                       flattenedAnswer.level + 1,
                       flattenedAnswer,
-                      flattenedAnswer.oneParentIsInactive
+                      flattenedAnswer.oneParentIsInactive,
+                      answerIndex
                     );
                   }
                 }
-              });
+              };
+
+              // determine how many answers we should generate
+              if (level === 0) {
+                this.value[flattenedQuestion.data.variable].forEach((_item, answerIndex) => {
+                  render(answerIndex);
+                });
+              } else {
+                render(answerParentIndex);
+              }
             }
 
             // finished
@@ -401,17 +438,6 @@ export class AppFormFillQuestionnaireV2Component
               };
             }
 
-            // flatten
-            const flattenedAnswer: IFlattenNodeAnswer = {
-              type: FlattenType.ANSWER,
-              level: flattenedQuestion.level + 1,
-              parent: flattenedQuestion,
-              oneParentIsInactive: (flattenedQuestion.data as QuestionModel).inactive || flattenedQuestion.oneParentIsInactive,
-              name: `${this.name}[${question.variable}]`,
-              index: 0,
-              definition
-            };
-
             // initialize value if necessary too
             if (
               !this.value[flattenedQuestion.data.variable] ||
@@ -422,8 +448,31 @@ export class AppFormFillQuestionnaireV2Component
               }];
             }
 
-            // add to list
-            this.flattenedQuestions.push(flattenedAnswer);
+            // render
+            const render = (answerIndex: number) => {
+              // flatten
+              const flattenedAnswer: IFlattenNodeAnswer = {
+                type: FlattenType.ANSWER,
+                level: flattenedQuestion.level + 1,
+                parent: flattenedQuestion,
+                oneParentIsInactive: (flattenedQuestion.data as QuestionModel).inactive || flattenedQuestion.oneParentIsInactive,
+                name: `${this.name}[${question.variable}][${answerIndex}].value`,
+                index: answerIndex,
+                definition
+              };
+
+              // add to list
+              this.flattenedQuestions.push(flattenedAnswer);
+            };
+
+            // determine how many answers we should generate
+            if (level === 0) {
+              this.value[flattenedQuestion.data.variable].forEach((_item, answerIndex) => {
+                render(answerIndex);
+              });
+            } else {
+              render(answerParentIndex);
+            }
 
             // finished
             break;
@@ -447,7 +496,10 @@ export class AppFormFillQuestionnaireV2Component
    */
   answersWithChildrenQuestionsChanged(): void {
     // update questionnaire
-    this.nonFlatToFlat(true);
+    this.nonFlatToFlat(
+      true,
+      false
+    );
 
     // make dirty
     this.onChange(this.value);
@@ -470,7 +522,123 @@ export class AppFormFillQuestionnaireV2Component
     }
 
     // redraw
-    this.nonFlatToFlat(true);
+    this.nonFlatToFlat(
+      true,
+      false
+    );
+  }
+
+  /**
+   * Fill out missing values
+   */
+  private fillOutValuesForMultiAnswerQuestion(question: QuestionModel): void {
+    // multi answer ?
+    if (!question.multiAnswer) {
+      return;
+    }
+
+    // add missing values
+    // - try to map by date if answers already exist, maybe answer on index 2 should actually be answer 3 while on 2 we should put empty because a parent intermediary response was changed at a specific time
+    const addMissingValues = (childQuestion: QuestionModel) => {
+      // must initialize ?
+      if (!this.value[childQuestion.variable]) {
+        this.value[childQuestion.variable] = [];
+      }
+
+      // if original variable we just need to fill out
+      if (childQuestion.variable === question.variable) {
+        // nothing to do since the number of items is the question ...answers length
+        // jump to children
+      } else {
+        // fill out with dates or empty
+        const oldValues = this.value[childQuestion.variable];
+        const numberOfValues: number = this.value[question.variable].length;
+        this.value[childQuestion.variable] = [];
+        for (let childIndex = 0; childIndex < numberOfValues; childIndex++) {
+          // get parent date
+          let dateFormatted = this.value[question.variable][childIndex].date;
+          const dateISO = dateFormatted ?
+            moment(dateFormatted).toISOString() :
+            undefined;
+          dateFormatted = dateFormatted ?
+            moment(dateFormatted).format(Constants.DEFAULT_DATE_DISPLAY_FORMAT) :
+            dateFormatted;
+
+          // if date provided, then search for child with same date
+          let answerIndexDeterminedByDate: number = oldValues
+            .findIndex((answer) => {
+              return (
+                !dateFormatted &&
+                !answer.date
+              ) || (
+                dateFormatted &&
+                answer.date &&
+                moment(answer.date).format(Constants.DEFAULT_DATE_DISPLAY_FORMAT) === dateFormatted
+              );
+            });
+
+          // found our item ?
+          if (answerIndexDeterminedByDate > -1) {
+            this.value[childQuestion.variable].push(oldValues.splice(answerIndexDeterminedByDate, 1)[0]);
+          } else {
+            // fill with next empty...
+            answerIndexDeterminedByDate = oldValues.findIndex((item) => !item.date);
+            if (answerIndexDeterminedByDate > -1) {
+              this.value[childQuestion.variable].push({
+                date: dateISO,
+                value: oldValues.splice(answerIndexDeterminedByDate, 1)[0].value
+              });
+            } else {
+              // fill with empty
+              this.value[childQuestion.variable].push({
+                date: dateISO,
+                value: undefined
+              });
+            }
+          }
+        }
+      }
+
+      // check children
+      (childQuestion.answers || []).forEach((childAnswer) => {
+        if (childAnswer.additionalQuestions?.length > 0) {
+          childAnswer.additionalQuestions.forEach((cQuestion) => {
+            addMissingValues(cQuestion);
+          });
+        }
+      });
+    };
+
+    // must initialize ?
+    if (!this.value[question.variable]) {
+      this.value[question.variable] = [];
+    }
+
+    // add missing indexes - used by multi-answer so responses from other questions match as index, otherwise they won't be matched properly after selection
+    // - if one child question appears when an answer is selected, if on one date we select it while on other we don't...the index won't match anymore
+    addMissingValues(question);
+  }
+
+  /**
+   * Add multi answer
+   */
+  addMultiAnswer(item: IFlattenNodeQuestion): void {
+    // #TODO
+    console.log(item);
+    // side dialog..to enter date..so we can fill children with that date
+    // or...inline..and update children when date changes..on the same index
+  }
+
+  /**
+   * Remove item
+   */
+  removeItem(item: IFlattenNodeQuestion): void {
+    // #TODO
+    // - remove answer from multi answer question... should remove child array items too
+    // the opposite of fillOutValuesForMultiAnswerQuestion
+
+    // #TODO
+    console.log(item);
   }
 
   /**
