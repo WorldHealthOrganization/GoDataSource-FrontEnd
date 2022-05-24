aa4b46956ab2c5df6b902f60c4c902bd0a6f1282
Adrian Morariu <adrian.morariu@clarisoft.com>
2022-05-24 10:53:19 +0300

    WGD-3567 PRIORITY: UI/UX Improvement

--- src/app/features/dashboard/components/index.ts
+++ src/app/features/dashboard/components/index.ts
@@ -2,7 +2,6 @@
 import { ContactsOnFollowupListDashletComponent } from './contacts-on-followup-list-dashlet/contacts-on-followup-list-dashlet.component';
 import { ContactsPerCaseMeanDashletComponent } from './contacts-per-case-mean-dashlet/contacts-per-case-mean-dashlet.component';
 import { ContactsPerCaseMedianDashletComponent } from './contacts-per-case-median-dashlet/contacts-per-case-median-dashlet.component';
-import { CasesDeceasedDashletComponent } from './cases-deceased-dashlet/cases-deceased-dashlet.component';
 import { CasesHospitalisedDashletComponent } from './cases-hospitalised-dashlet/cases-hospitalised-dashlet.component';
 import { CasesLessContactsDashletComponent } from './cases-less-contacts-dashlet/cases-less-contacts-dashlet.component';
 import { ContactsLostToFollowUpDashletComponent } from './contacts-lost-to-follow-up-dashlet/contacts-lost-to-follow-up-dashlet.component';
@@ -37,7 +36,6 @@ export const components: any[] = [
   AppKpiDashletComponent,
 
   ContactsOnFollowupListDashletComponent,
-  CasesDeceasedDashletComponent,
   CasesHospitalisedDashletComponent,
   ContactsPerCaseMeanDashletComponent,
   ContactsPerCaseMedianDashletComponent,
