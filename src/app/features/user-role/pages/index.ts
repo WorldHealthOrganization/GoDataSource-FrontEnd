c6f8db3e806c97c78a0438f31e75368d3d2c5685
Adrian Morariu <adrian.morariu@clarisoft.com>
2022-05-28 20:38:49 +0300

    WGD-3567 PRIORITY: UI/UX Improvement

--- src/app/features/user-role/pages/index.ts
+++ src/app/features/user-role/pages/index.ts
@@ -1,16 +1,13 @@
 // import each page component
 import { RolesListComponent } from './roles-list/roles-list.component';
-import { CreateRoleComponent } from './create-role/create-role.component';
-import { ModifyRoleComponent } from './modify-role/modify-role.component';
+import { RolesCreateViewModifyComponent } from './roles-create-view-modify/roles-create-view-modify.component';
 
 // export each page component individually
+export * from './roles-create-view-modify/roles-create-view-modify.component';
 export * from './roles-list/roles-list.component';
-export * from './create-role/create-role.component';
-export * from './modify-role/modify-role.component';
 
 // export the list of all page components
 export const pageComponents: any[] = [
-  RolesListComponent,
-  CreateRoleComponent,
-  ModifyRoleComponent
+  RolesCreateViewModifyComponent,
+  RolesListComponent
 ];
