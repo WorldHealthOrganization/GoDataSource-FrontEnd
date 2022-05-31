4475ae9bf1c463dac434dd2c84774f51c1e138b4
Adrian Morariu <adrian.morariu@clarisoft.com>
2022-05-31 11:47:14 +0300

    WGD-3567 PRIORITY: UI/UX Improvement

--- src/app/core/services/resolvers/data/person.resolver.ts
+++ src/app/core/services/resolvers/data/person.resolver.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 import { ToastV2Service } from '../../helper/toast-v2.service';
 import { CaseModel } from '../../../models/case.model';
 import { ContactModel } from '../../../models/contact.model';
@@ -12,6 +12,7 @@ import { ContactDataService } from '../../data/contact.data.service';
 import { CaseDataService } from '../../data/case.data.service';
 import { EventDataService } from '../../data/event.data.service';
 import { ContactsOfContactsDataService } from '../../data/contacts-of-contacts.data.service';
+import { RequestQueryBuilder } from '../../../helperClasses/request-query-builder';
 
 @Injectable()
 export class PersonDataResolver implements Resolve<CaseModel | ContactModel | EventModel | ContactOfContactModel> {
@@ -32,54 +33,84 @@ export class PersonDataResolver implements Resolve<CaseModel | ContactModel | Ev
    */
   resolve(route): Observable<CaseModel | ContactModel | EventModel | ContactOfContactModel> {
     // not found
-    let request: Observable<CaseModel | ContactModel | EventModel | ContactOfContactModel> = of(null);
+    let request: Observable<CaseModel[] | ContactModel[] | EventModel[] | ContactOfContactModel[]>;
+
+    // determine id
+    const id: string = route.params.contactId ||
+      route.queryParams.contactId ||
+      route.params.caseId ||
+      route.queryParams.caseId ||
+      route.params.eventId ||
+      route.queryParams.eventId ||
+      route.params.contactOfContactId ||
+      route.queryParams.contactOfContactId;
+
+    // nothing to retrieve ?
+    if (!id) {
+      return of(null);
+    }
+
+    // construct query builder to include deleted records
+    const qb = new RequestQueryBuilder();
+
+    // retrieve our record
+    qb.filter.where({
+      id
+    });
+
+    // include deleted
+    qb.includeDeleted();
+
+    // we can't have more than 1 with same id :)
+    qb.limit(1);
 
     // contact ?
-    if (route.params.contactId) {
-      request = this.contactDataService.getContact(
+    if (
+      route.params.contactId ||
+      route.queryParams.contactId
+    ) {
+      request = this.contactDataService.getContactsList(
         this.storageService.get(StorageKey.SELECTED_OUTBREAK_ID),
-        route.params.contactId
+        qb
       );
-    } else if (route.queryParams.contactId) {
-      request = this.contactDataService.getContact(
+    } else if (
+      route.params.caseId ||
+      route.queryParams.caseId
+    ) {
+      // request
+      request = this.caseDataService.getCasesList(
         this.storageService.get(StorageKey.SELECTED_OUTBREAK_ID),
-        route.queryParams.contactId
+        qb
       );
-    } else if (route.params.caseId) {
-      request = this.caseDataService.getCase(
+    } else if (
+      route.params.eventId ||
+      route.queryParams.eventId
+    ) {
+      // request
+      request = this.eventDataService.getEventsList(
         this.storageService.get(StorageKey.SELECTED_OUTBREAK_ID),
-        route.params.caseId
+        qb
       );
-    } else if (route.queryParams.caseId) {
-      request = this.caseDataService.getCase(
+    } else if (
+      route.params.contactOfContactId ||
+      route.queryParams.contactOfContactId
+    ) {
+      // request
+      request = this.contactsOfContactsDataService.getContactsOfContactsList(
         this.storageService.get(StorageKey.SELECTED_OUTBREAK_ID),
-        route.queryParams.caseId
-      );
-    } else if (route.params.eventId) {
-      request = this.eventDataService.getEvent(
-        this.storageService.get(StorageKey.SELECTED_OUTBREAK_ID),
-        route.params.eventId
-      );
-    } else if (route.queryParams.eventId) {
-      request = this.eventDataService.getEvent(
-        this.storageService.get(StorageKey.SELECTED_OUTBREAK_ID),
-        route.queryParams.eventId
-      );
-    } else if (route.params.contactOfContactId) {
-      request = this.contactsOfContactsDataService.getContactOfContact(
-        this.storageService.get(StorageKey.SELECTED_OUTBREAK_ID),
-        route.params.contactOfContactId
-      );
-    } else if (route.queryParams.contactOfContactId) {
-      request = this.contactsOfContactsDataService.getContactOfContact(
-        this.storageService.get(StorageKey.SELECTED_OUTBREAK_ID),
-        route.queryParams.contactOfContactId
+        qb
       );
     }
 
     // retrieve
     return request
       .pipe(
+        map((data) => {
+          return data?.length > 0 ?
+            data[0] :
+            null;
+        }),
+
         // should be last one
         catchError((err) => {
           // display error
