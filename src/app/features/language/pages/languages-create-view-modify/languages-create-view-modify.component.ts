33ab20de8e4a5769c38a81a340d2c11693bfe907
Adrian Morariu <adrian.morariu@clarisoft.com>
2022-06-01 15:57:40 +0300

    WGD-3944 Implement new UI for "Languages" list pages

---
+++ src/app/features/language/pages/languages-create-view-modify/languages-create-view-modify.component.ts
@@ -0,0 +1,389 @@
+import { Component, OnDestroy, Renderer2 } from '@angular/core';
+import { CreateViewModifyComponent } from '../../../../core/helperClasses/create-view-modify-component';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DashboardModel } from '../../../../core/models/dashboard.model';
+import { AuthDataService } from '../../../../core/services/data/auth.data.service';
+import { Observable, throwError } from 'rxjs';
+import { ToastV2Service } from '../../../../core/services/helper/toast-v2.service';
+import { TranslateService } from '@ngx-translate/core';
+import {
+  CreateViewModifyV2ActionType,
+  CreateViewModifyV2MenuType,
+  CreateViewModifyV2TabInputType,
+  ICreateViewModifyV2Buttons,
+  ICreateViewModifyV2CreateOrUpdate,
+  ICreateViewModifyV2Tab
+} from '../../../../shared/components-v2/app-create-view-modify-v2/models/tab.model';
+import { CreateViewModifyV2ExpandColumnType } from '../../../../shared/components-v2/app-create-view-modify-v2/models/expand-column.model';
+import { RedirectService } from '../../../../core/services/helper/redirect.service';
+import { RequestFilterGenerator } from '../../../../core/helperClasses/request-query-builder';
+import { catchError, takeUntil } from 'rxjs/operators';
+import { DialogV2Service } from '../../../../core/services/helper/dialog-v2.service';
+import { LanguageModel } from '../../../../core/models/language.model';
+import { LanguageDataService } from '../../../../core/services/data/language.data.service';
+import { ClusterModel } from '../../../../core/models/cluster.model';
+
+/**
+ * Component
+ */
+@Component({
+  selector: 'app-languages-create-view-modify',
+  templateUrl: './languages-create-view-modify.component.html'
+})
+export class LanguagesCreateViewModifyComponent extends CreateViewModifyComponent<LanguageModel> implements OnDestroy {
+  /**
+   * Constructor
+   */
+  constructor(
+    protected activatedRoute: ActivatedRoute,
+    protected toastV2Service: ToastV2Service,
+    protected translateService: TranslateService,
+    protected router: Router,
+    protected dialogV2Service: DialogV2Service,
+    protected languageDataService: LanguageDataService,
+    authDataService: AuthDataService,
+    renderer2: Renderer2,
+    redirectService: RedirectService
+  ) {
+    super(
+      toastV2Service,
+      renderer2,
+      redirectService,
+      activatedRoute,
+      authDataService
+    );
+  }
+
+  /**
+   * Release resources
+   */
+  ngOnDestroy(): void {
+    // parent
+    super.onDestroy();
+  }
+
+  /**
+   * Create new item model if needed
+   */
+  protected createNewItem(): LanguageModel {
+    return new LanguageModel();
+  }
+
+  /**
+   * Retrieve item
+   */
+  protected retrieveItem(record?: LanguageModel): Observable<LanguageModel> {
+    return this.languageDataService
+      .getLanguage(
+        record ?
+          record.id :
+          this.activatedRoute.snapshot.params.languageId
+      );
+  }
+
+  /**
+   * Data initialized
+   */
+  protected initializedData(): void {}
+
+  /**
+   * Initialize page title
+   */
+  protected initializePageTitle(): void {
+    // add info accordingly to page type
+    if (this.isCreate) {
+      this.pageTitle = 'LNG_PAGE_CREATE_LANGUAGE_TITLE';
+      this.pageTitleData = undefined;
+    } else if (this.isModify) {
+      this.pageTitle = 'LNG_PAGE_MODIFY_LANGUAGE_TITLE';
+      this.pageTitleData = {
+        name: this.itemData.name
+      };
+    } else {
+      // view
+      this.pageTitle = 'LNG_PAGE_VIEW_LANGUAGE_TITLE';
+      this.pageTitleData = {
+        name: this.itemData.name
+      };
+    }
+  }
+
+  /**
+   * Initialize breadcrumbs
+   */
+  protected initializeBreadcrumbs() {
+    // reset breadcrumbs
+    this.breadcrumbs = [
+      {
+        label: 'LNG_COMMON_LABEL_HOME',
+        action: {
+          link: DashboardModel.canViewDashboard(this.authUser) ?
+            ['/dashboard'] :
+            ['/account/my-profile']
+        }
+      }
+    ];
+
+    // list page
+    if (LanguageModel.canList(this.authUser)) {
+      this.breadcrumbs.push({
+        label: 'LNG_PAGE_LIST_LANGUAGES_TITLE',
+        action: {
+          link: ['/languages']
+        }
+      });
+    }
+
+    // add info accordingly to page type
+    if (this.isCreate) {
+      this.breadcrumbs.push({
+        label: 'LNG_PAGE_CREATE_LANGUAGE_TITLE',
+        action: null
+      });
+    } else if (this.isModify) {
+      this.breadcrumbs.push({
+        label: this.translateService.instant(
+          'LNG_PAGE_MODIFY_LANGUAGE_TITLE', {
+            name: this.itemData.name
+          }
+        ),
+        action: null
+      });
+    } else {
+      // view
+      this.breadcrumbs.push({
+        label: this.translateService.instant(
+          'LNG_PAGE_VIEW_LANGUAGE_TITLE', {
+            name: this.itemData.name
+          }
+        ),
+        action: null
+      });
+    }
+  }
+
+  /**
+   * Initialize tabs
+   */
+  protected initializeTabs(): void {
+    this.tabData = {
+      // tabs
+      tabs: [
+        // Personal
+        this.initializeTabsDetails()
+      ],
+
+      // create details
+      create: {
+        finalStep: {
+          buttonLabel: this.translateService.instant('LNG_PAGE_CREATE_LANGUAGE_ACTION_CREATE_LANGUAGE_BUTTON'),
+          message: () => this.translateService.instant(
+            'LNG_STEPPER_FINAL_STEP_TEXT_GENERAL',
+            this.itemData
+          )
+        }
+      },
+
+      // buttons
+      buttons: this.initializeButtons(),
+
+      // create or update
+      createOrUpdate: this.initializeProcessData(),
+      redirectAfterCreateUpdate: (data: LanguageModel) => {
+        // redirect to view
+        this.router.navigate([
+          '/languages',
+          data.id,
+          'view'
+        ]);
+      }
+    };
+  }
+
+  /**
+   * Initialize tabs - Details
+   */
+  private initializeTabsDetails(): ICreateViewModifyV2Tab {
+    return {
+      type: CreateViewModifyV2TabInputType.TAB,
+      label: this.isCreate ?
+        'LNG_PAGE_CREATE_LANGUAGE_TAB_DETAILS_TITLE' :
+        'LNG_PAGE_MODIFY_LANGUAGE_TAB_DETAILS_TITLE',
+      sections: [
+        // Details
+        {
+          type: CreateViewModifyV2TabInputType.SECTION,
+          label: this.isCreate ?
+            'LNG_PAGE_CREATE_LANGUAGE_TAB_DETAILS_TITLE' :
+            'LNG_PAGE_MODIFY_LANGUAGE_TAB_DETAILS_TITLE',
+          inputs: [
+            {
+              type: CreateViewModifyV2TabInputType.TEXT,
+              name: 'name',
+              placeholder: () => 'LNG_LANGUAGE_FIELD_LABEL_NAME',
+              description: () => 'LNG_LANGUAGE_FIELD_LABEL_NAME_DESCRIPTION',
+              value: {
+                get: () => this.itemData.name,
+                set: (value) => {
+                  // set data
+                  this.itemData.name = value;
+                }
+              },
+              validators: {
+                required: () => true
+              }
+            }
+          ]
+        }
+      ]
+    };
+  }
+
+  /**
+   * Initialize buttons
+   */
+  private initializeButtons(): ICreateViewModifyV2Buttons {
+    return {
+      view: {
+        link: {
+          link: () => ['/languages', this.itemData?.id, 'view']
+        }
+      },
+      modify: this.itemData.readOnly ? undefined : {
+        link: {
+          link: () => ['/languages', this.itemData?.id, 'modify']
+        },
+        visible: () => LanguageModel.canModify(this.authUser)
+      },
+      createCancel: {
+        link: {
+          link: () => ['/languages']
+        }
+      },
+      viewCancel: {
+        link: {
+          link: () => ['/languages']
+        }
+      },
+      modifyCancel: {
+        link: {
+          link: () => ['/languages']
+        }
+      },
+      quickActions: {
+        options: [
+          // Record details
+          {
+            type: CreateViewModifyV2MenuType.OPTION,
+            label: 'LNG_COMMON_LABEL_DETAILS',
+            action: {
+              click: () => {
+                // show record details dialog
+                this.dialogV2Service.showRecordDetailsDialog(
+                  'LNG_COMMON_LABEL_DETAILS',
+                  this.itemData,
+                  this.activatedRoute.snapshot.data.user
+                );
+              }
+            },
+            visible: () => !this.isCreate
+          }
+        ]
+      }
+    };
+  }
+
+  /**
+   * Initialize process data
+   */
+  private initializeProcessData(): ICreateViewModifyV2CreateOrUpdate {
+    return (
+      type,
+      data,
+      finished,
+      _loading,
+      _forms
+    ) => {
+      // finished
+      (type === CreateViewModifyV2ActionType.CREATE ?
+        this.languageDataService.createLanguage(
+          data
+        ) :
+        this.languageDataService.modifyLanguage(
+          this.itemData.id,
+          data
+        )
+      ).pipe(
+        // handle error
+        catchError((err) => {
+          // show error
+          finished(err, undefined);
+
+          // finished
+          return throwError(err);
+        }),
+
+        // should be the last pipe
+        takeUntil(this.destroyed$)
+      ).subscribe((item: ClusterModel) => {
+        // success creating / updating cluster
+        this.toastV2Service.success(
+          type === CreateViewModifyV2ActionType.CREATE ?
+            'LNG_PAGE_CREATE_LANGUAGE_ACTION_CREATE_LANGUAGE_SUCCESS_MESSAGE' :
+            'LNG_PAGE_MODIFY_LANGUAGE_ACTION_MODIFY_LANGUAGE_SUCCESS_MESSAGE'
+        );
+
+        // finished with success
+        finished(undefined, item);
+      });
+    };
+  }
+
+  /**
+   * Initialize expand list column renderer fields
+   */
+  protected initializeExpandListColumnRenderer(): void {
+    this.expandListColumnRenderer = {
+      type: CreateViewModifyV2ExpandColumnType.TEXT,
+      get: (item: LanguageModel) => item.name,
+      link: (item: LanguageModel) => ['/languages', item.id, 'view']
+    };
+  }
+
+  /**
+   * Initialize expand list query fields
+   */
+  protected initializeExpandListQueryFields(): void {
+    this.expandListQueryFields = [
+      'id',
+      'name'
+    ];
+  }
+
+  /**
+   * Initialize expand list advanced filters
+   */
+  protected initializeExpandListAdvancedFilters(): void {}
+
+  /**
+   * Refresh expand list
+   */
+  refreshExpandList(data): void {
+    // append / remove search
+    if (data.searchBy) {
+      data.queryBuilder.filter.where({
+        name: RequestFilterGenerator.textContains(
+          data.searchBy
+        )
+      });
+    }
+
+    // retrieve data
+    this.expandListRecords$ = this.languageDataService
+      .getLanguagesList(data.queryBuilder)
+      .pipe(
+        // should be the last pipe
+        takeUntil(this.destroyed$)
+      );
+  }
+}
