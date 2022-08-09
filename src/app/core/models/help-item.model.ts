df9c42e82d781f3165bb9e55631b0635d2b762dc
Adrian Morariu <adrian.morariu@clarisoft.com>
2022-08-09 18:29:47 +0300

    WGD-3858 [Service] Create help items adds token in description

--- src/app/core/models/help-item.model.ts
+++ src/app/core/models/help-item.model.ts
@@ -44,7 +44,7 @@ export class HelpItemModel extends BaseModel
 
     this.id = _.get(data, 'id');
     this.title = _.get(data, 'title');
-    this.content = _.get(data, 'content');
+    this.content = _.get(data, 'content', '');
     this.categoryId = _.get(data, 'categoryId');
     this.approved = _.get(data, 'approved', false);
     this.approvedBy = _.get(data, 'approvedBy');
