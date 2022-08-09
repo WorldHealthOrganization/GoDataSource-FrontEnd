97d1d883c505629b0eaf95e89050f6a45ba745e9
Adrian Morariu <adrian.morariu@clarisoft.com>
2022-08-09 13:57:38 +0300

    Remove unused components

--- src/app/shared/xt-forms/components/index.ts
+++ src/app/shared/xt-forms/components/index.ts
@@ -1,15 +1,9 @@
 import { FormValidationComponent } from './form-validation/form-validation.component';
-import { FormTextareaComponent } from './form-textarea/form-textarea.component';
-import { FormSlideToggleComponent } from './form-slide-toggle/form-slide-toggle.component';
 import { FormCheckboxComponent } from './form-checkbox/form-checkbox.component';
 import { FormSelectVscrollComponent } from './form-select-vscroll/form-select-vscroll.component';
-import { FormTimepickerComponent } from './form-timepicker/form-timepicker.component';
 
 export const components: any[] = [
   FormValidationComponent,
-  FormTextareaComponent,
   FormSelectVscrollComponent,
-  FormSlideToggleComponent,
-  FormCheckboxComponent,
-  FormTimepickerComponent
+  FormCheckboxComponent
 ];
