5b2710292d0ddfd0b470e0b5ba495b124501eeec
ionut-mocan <ionut.mocan@clarisoft.com>
2022-08-10 14:24:27 +0300

    WGD-4033: Changed user workload page to design V2

--- src/app/features/user/pages/user-workload/user-workload.component.ts
+++ src/app/features/user/pages/user-workload/user-workload.component.ts
@@ -1,70 +1,67 @@
-import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { ListComponent } from '../../../../core/helperClasses/list-component';
 import { FollowUpsDataService } from '../../../../core/services/data/follow-ups.data.service';
-import { OutbreakDataService } from '../../../../core/services/data/outbreak.data.service';
-import { OutbreakModel } from '../../../../core/models/outbreak.model';
-import { UserFollowupsPerDayModel } from '../../../../core/models/user-followups-per-day.model';
 import { Constants } from '../../../../core/models/constants';
 import { I18nService } from '../../../../core/services/helper/i18n.service';
-import { UserDataService } from '../../../../core/services/data/user.data.service';
 import * as _ from 'lodash';
-import { Subscription } from 'rxjs';
 import { Moment, moment } from '../../../../core/helperClasses/x-moment';
 import { UserModel } from '../../../../core/models/user.model';
-import { catchError } from 'rxjs/operators';
-import { throwError } from 'rxjs/internal/observable/throwError';
+import { map, takeUntil, tap } from 'rxjs/operators';
 import { ListHelperService } from '../../../../core/services/helper/list-helper.service';
-import { ToastV2Service } from '../../../../core/services/helper/toast-v2.service';
-
-interface IUserMap {
-  id: string;
-  dates: {};
-  name: string;
-}
+import { DashboardModel } from '../../../../core/models/dashboard.model';
+import { V2AdvancedFilterComparatorType, V2AdvancedFilterType } from '../../../../shared/components-v2/app-list-table-v2/models/advanced-filter.model';
+import { ActivatedRoute } from '@angular/router';
+import { IV2Column, IV2ColumnPinned, V2ColumnFormat } from '../../../../shared/components-v2/app-list-table-v2/models/column.model';
+import { Location } from '@angular/common';
+import { IV2DateRange } from '../../../../shared/forms-v2/components/app-form-date-range-v2/models/date.model';
+import { SavedFilterData, SavedFilterDataAppliedFilter } from '../../../../core/models/saved-filters.model';
+import { RequestFilterOperator } from '../../../../core/helperClasses/request-query-builder';
 
 @Component({
   selector: 'app-user-workload',
-  encapsulation: ViewEncapsulation.None,
-  templateUrl: './user-workload.component.html',
-  styleUrls: ['./user-workload.component.less']
+  templateUrl: './user-workload.component.html'
 })
-export class UserWorkloadComponent extends ListComponent<any> implements OnInit, OnDestroy {
-  // breadcrumbs
-  // breadcrumbs: BreadcrumbItemModel[] = [];
+export class UserWorkloadComponent extends ListComponent<any> implements OnDestroy {
+  // default table columns
+  defaultTableColumns: IV2Column[] = [
+    {
+      field: 'user',
+      label: 'LNG_PAGE_USERS_WORKLOAD_USER_LABEL',
+      pinned: IV2ColumnPinned.LEFT,
+      notMovable: true,
+      format: {
+        type: 'user.name'
+      },
+      link: (data) => {
+        return UserModel.canView(this.authUser) && this.activatedRoute.snapshot.data.user.map[data.user.id] ?
+          `/users/${ data.user.id }/view` :
+          undefined;
+      }
+    }
+  ];
 
-  dates: string[] = [];
-  usersDataShow: IUserMap[] = [];
-  usersData: IUserMap[];
-
-  // loading flag - display spinner instead of table
-  displayLoading: boolean = false;
-
-  // Filter slider data
-  slideFilterData: {
-    minDate: Moment,
-    maxDate: Moment,
-    maxRange: number
+  // advanced filters config
+  advancedFiltersConfig: {
+    operatorHide?: boolean,
+    disableAdd?: boolean,
+    disableReset?: boolean,
+    disableDelete?: boolean
   } = {
-      minDate: moment().startOf('day'),
-      maxDate: moment().endOf('day'),
-      maxRange: 0
+      operatorHide: true,
+      disableAdd: true,
+      disableReset: true,
+      disableDelete: true
     };
-
-  // Slider Date Filter Value
-  sliderDateFilterValue: any; // FormDateRangeSliderData;
-
-  getSelectedOutbreakSubject: Subscription;
 
   /**
      * Constructor
      */
   constructor(
     protected listHelperService: ListHelperService,
-    private outbreakDataService: OutbreakDataService,
     private followUpsDataService: FollowUpsDataService,
-    private toastV2Service: ToastV2Service,
     private i18nService: I18nService,
-    private userDataService: UserDataService
+    private activatedRoute: ActivatedRoute,
+    private location: Location
   ) {
     super(
       listHelperService,
@@ -73,88 +70,47 @@ export class UserWorkloadComponent extends ListComponent<any> implements OnInit,
   }
 
   /**
-     * Component initialized
-     */
-  ngOnInit() {
-    // get users
-    this.displayLoading = true;
-    this.userDataService
-      .getUsersList()
-      .subscribe((users) => {
-        // map users
-        this.usersData = [{
-          id: null,
-          name: this.i18nService.instant('LNG_PAGE_USERS_WORKLOAD_NO_USER_LABEL'),
-          dates: {}
-        }];
-        _.forEach(users, (user: UserModel) => {
-          this.usersData.push({
-            id: user.id,
-            name: user.name,
-            dates: {}
-          });
-        });
-
-        // retrieve outbreak data
-        this.getSelectedOutbreakSubject = this.outbreakDataService
-          .getSelectedOutbreakSubject()
-          .subscribe((selectedOutbreak: OutbreakModel) => {
-            // selected outbreak
-            this.selectedOutbreak = selectedOutbreak;
-            if (
-              this.selectedOutbreak &&
-                            this.selectedOutbreak.id
-            ) {
-              // set min & max dates
-              this.slideFilterData.minDate = moment(this.selectedOutbreak.startDate).startOf('day');
-              this.slideFilterData.maxDate = moment().add(1, 'days').endOf('day');
-              this.slideFilterData.maxRange = this.selectedOutbreak.periodOfFollowup;
-              // this.sliderDateFilterValue = new FormDateRangeSliderData({
-              //   low: moment().add(-this.selectedOutbreak.periodOfFollowup + 1, 'days').startOf('day'),
-              //   high: moment().add(1, 'days').endOf('day')
-              // });
-              this.sliderDateFilterValue = {
-                low: moment().add(-this.selectedOutbreak.periodOfFollowup + 1, 'days').startOf('day'),
-                high: moment().add(1, 'days').endOf('day')
-              };
-            } else {
-              // hide loading
-              this.displayLoading = false;
-            }
-          });
-      });
-
-    // initialize breadcrumbs
-    this.initializeBreadcrumbs();
-  }
-
-  /**
-     * Initialize breadcrumbs
-     */
-  // private initializeBreadcrumbs() {
-  //   // reset
-  //   this.breadcrumbs = [];
-  //
-  //   // add list breadcrumb only if we have permission
-  //   if (UserModel.canList(this.authUser)) {
-  //     this.breadcrumbs.push(new BreadcrumbItemModel('LNG_PAGE_LIST_USERS_TITLE', '/users'));
-  //   }
-  //
-  //   // workload breadcrumb
-  //   this.breadcrumbs.push(new BreadcrumbItemModel('LNG_PAGE_USERS_WORKLOAD_TITLE', '.', true));
-  // }
-
-  /**
      * Remove component resources
      */
   ngOnDestroy() {
     // release parent resources
     super.onDestroy();
+  }
 
-    if (this.getSelectedOutbreakSubject) {
-      this.getSelectedOutbreakSubject.unsubscribe();
-      this.getSelectedOutbreakSubject = null;
-    }
+  /**
+   * Selected outbreak was changed
+   */
+  selectedOutbreakChanged(): void {
+    // set default advanced filters
+    const defaultFilter: IV2DateRange = {
+      startDate: moment(Constants.getCurrentDate().subtract(28, 'days')).startOf('day'),
+      endDate: moment(Constants.getCurrentDate()).endOf('day')
+    };
+    this.tableV2Component.generateFiltersFromFilterData(new SavedFilterData({
+      appliedFilterOperator: RequestFilterOperator.AND,
+      appliedFilters: [
+        // date
+        new SavedFilterDataAppliedFilter({
+          filter: {
+            uniqueKey: 'dateLNG_PAGE_USERS_WORKLOAD_DATE_LABEL'
+          },
+          comparator: V2AdvancedFilterComparatorType.BETWEEN,
+          value: defaultFilter
+        })
+      ]
+    }));
+
+    // filter by default range
+    this.queryBuilder.filter.byDateRange(
+      'date',
+      defaultFilter
+    );
+
+    // initialize pagination
+    // this page doesn't have pagination
+
+    // ...and re-load the list when the Selected Outbreak is changed
+    this.needsRefreshList(true);
   }
 
   /**
@@ -170,12 +126,24 @@ export class UserWorkloadComponent extends ListComponent<any> implements OnInit,
   /**
    * Initialize table infos
    */
-  protected initializeTableInfos(): void {}
+  protected initializeTableInfos(): void {
+    this.infos = [
+      'LNG_PAGE_USERS_WORKLOAD_NO_DATA_LABEL'
+    ];
+  }
 
   /**
    * Initialize Table Advanced Filters
    */
-  protected initializeTableAdvancedFilters(): void {}
+  protected initializeTableAdvancedFilters(): void {
+    this.advancedFilters = [
+      {
+        type: V2AdvancedFilterType.RANGE_DATE,
+        field: 'date',
+        label: 'LNG_PAGE_USERS_WORKLOAD_DATE_LABEL'
+      }
+    ];
+  }
 
   /**
    * Initialize table quick actions
@@ -201,6 +169,31 @@ export class UserWorkloadComponent extends ListComponent<any> implements OnInit,
    * Initialize breadcrumbs
    */
   initializeBreadcrumbs(): void {
+    // reset
+    this.breadcrumbs = [{
+      label: 'LNG_COMMON_LABEL_HOME',
+      action: {
+        link: DashboardModel.canViewDashboard(this.authUser) ?
+          ['/dashboard'] :
+          ['/account/my-profile']
+      }
+    }];
+
+    // list page
+    if (UserModel.canList(this.authUser)) {
+      this.breadcrumbs.push({
+        label: 'LNG_PAGE_LIST_USERS_TITLE',
+        action: {
+          link: ['/users']
+        }
+      });
+    }
+
+    // current page
+    this.breadcrumbs.push({
+      label: 'LNG_PAGE_USERS_WORKLOAD_TITLE',
+      action: null
+    });
   }
 
   /**
@@ -211,122 +204,154 @@ export class UserWorkloadComponent extends ListComponent<any> implements OnInit,
   }
 
   /**
-   * Refresh list
-   */
+  * Refresh list
+  */
   refreshList() {
-    if (
-      this.selectedOutbreak &&
-            !_.isEmpty(this.usersData)
-    ) {
-      // construct array of dates
-      this.displayLoading = true;
-      this.dates = [];
-      const dates = [
-        this.i18nService.instant('LNG_PAGE_USERS_WORKLOAD_USER_LABEL')
-      ];
-      const currentDate = moment(this.sliderDateFilterValue.low);
-      while (currentDate.isSameOrBefore(this.sliderDateFilterValue.high)) {
-        dates.push(currentDate.format(Constants.DEFAULT_DATE_DISPLAY_FORMAT));
-        currentDate.add(1, 'days');
-      }
+    // retrieve the list of Follow Ups
+    this.records$ = this.followUpsDataService
+      .getFollowUpsPerDayUser(this.selectedOutbreak.id, this.queryBuilder)
+      .pipe(
+        // process data
+        map((metricUsersFollowups) => {
+          // Move "No team" to top
+          metricUsersFollowups.users.sort((a) => a.id ? 0 : -1);
 
-      // retrieve data
-      if (dates.length > 1) {
-        // add filter period
-        this.queryBuilder.filter.byDateRange(
-          'date', {
-            startDate: moment(this.sliderDateFilterValue.low).startOf('day'),
-            endDate: moment(this.sliderDateFilterValue.high).endOf('day')
-          }
-        );
+          // determine date ranges
+          let minDate: Moment;
+          let maxDate: Moment;
 
-        // retrieve the list of Follow Ups
-        this.followUpsDataService
-          .getFollowUpsPerDayUser(this.selectedOutbreak.id, this.queryBuilder)
-          .pipe(
-            catchError((err) => {
-              // hide loading
-              this.displayLoading = false;
+          // transform to list
+          const usedDates: {
+            [date: string]: true
+          } = {};
 
-              this.toastV2Service.error(err);
-              return throwError(err);
-            })
-          )
-          .subscribe((metricUsersFollowups: UserFollowupsPerDayModel) => {
-            // set headers
-            this.dates = dates;
+          // group by user
+          const followUpsGroupedByUser: {
+            user: UserModel,
+            followUpsPerDay: {
+              [date: string]: {
+                date: string | Moment,
+                contactIDs: string[],
+                totalFollowupsCount: number,
+                successfulFollowupsCount: number
+              }
+            }
+          }[] = (metricUsersFollowups.users || []).map((user) => {
+            // get grouped followups by team
+            return {
+              user: this.activatedRoute.snapshot.data.user.map[user.id] ??
+              {
+                name: this.i18nService.instant('LNG_PAGE_USERS_WORKLOAD_NO_USER_LABEL')
+              },
+              followUpsPerDay: _.keyBy(user.dates, (entry) => {
+                // determine min & max dates
+                const date = moment(entry.date).startOf('day');
+                minDate = minDate ?
+                  (date.isBefore(minDate) ? date : minDate) :
+                  date;
+                maxDate = maxDate ?
+                  (date.isAfter(maxDate) ? moment(date) : maxDate) :
+                  moment(date);
 
-            // format data
-            this.formatData(metricUsersFollowups);
-          });
-      } else {
-        // hide loading
-        this.displayLoading = false;
-      }
-    } else {
-      // hide loading
-      this.displayLoading = false;
-    }
-  }
+                // mark date found
+                usedDates[date.format(Constants.DEFAULT_DATE_DISPLAY_FORMAT)] = true;
 
-  /**
-     * Format the data
-     */
-  formatData(metricUsersFollowups: UserFollowupsPerDayModel) {
-    // format received data
-    if (
-      !_.isEmpty(this.usersData) &&
-            !_.isEmpty(metricUsersFollowups)
-    ) {
-      // map users for user search
-      const usersMap = {};
-      this.usersData.forEach((userData) => {
-        usersMap[userData.id ? userData.id : 'N'] = _.cloneDeep(userData);
-      });
-
-      // go through users and create list of date information
-      _.forEach(metricUsersFollowups.users, (user) => {
-        // construct list of dates
-        const dates = {};
-        if (user.dates) {
-          _.forEach(user.dates, (date) => {
-            dates[moment(date.date).format(Constants.DEFAULT_DATE_DISPLAY_FORMAT)] = {
-              totalFollowupsCount: date.totalFollowupsCount,
-              successfulFollowupsCount: date.successfulFollowupsCount
+                // set keys to dates
+                return [date.format(Constants.DEFAULT_DATE_DISPLAY_FORMAT)];
+              })
             };
           });
-        }
 
-        // assign dates
-        if (
-          user.id &&
-                    usersMap[user.id]
-        ) {
-          usersMap[user.id].dates = dates;
-        } else {
-          usersMap['N'].dates = dates;
-        }
-      });
+          // create dates array
+          const daysColumns: IV2Column[] = [];
+          if (
+            minDate &&
+            maxDate
+          ) {
+            // push dates
+            while (minDate.isSameOrBefore(maxDate)) {
+              // - exclude dates with no data
+              const formattedDate = minDate.format(Constants.DEFAULT_DATE_DISPLAY_FORMAT);
+              if (usedDates[formattedDate]) {
+                daysColumns.push({
+                  field: formattedDate,
+                  label: formattedDate,
+                  notMovable: true,
+                  format: {
+                    type: V2ColumnFormat.HTML
+                  },
+                  html: (
+                    data: {
+                      user: UserModel,
+                      followUpsPerDay: {
+                        date: string | Moment,
+                        contactIDs: string[],
+                        totalFollowupsCount: number,
+                        successfulFollowupsCount: number
+                      }
+                    },
+                    column
+                  ) => {
+                    // nothing to do here ?
+                    const followUpsPerDay = data.followUpsPerDay[column.field];
+                    if (!followUpsPerDay) {
+                      return '';
+                    }
 
-      // set data to show
-      this.usersDataShow = _.filter(usersMap, (v: IUserMap) => {
-        return !_.isEmpty(v.dates);
-      });
-    }
+                    // construct url
+                    const url: string = `/contacts/follow-ups?fromWorkload=true&date=${ moment(followUpsPerDay.date).format(Constants.DEFAULT_DATE_DISPLAY_FORMAT) }&user=${ data.user && data.user.id ? data.user.id : '' }`;
 
-    // hide loading
-    this.displayLoading = false;
-  }
+                    // status for successful followups
+                    const status: string = '&status=' + encodeURIComponent(JSON.stringify([
+                      'LNG_REFERENCE_DATA_CONTACT_DAILY_FOLLOW_UP_STATUS_TYPE_SEEN_OK',
+                      'LNG_REFERENCE_DATA_CONTACT_DAILY_FOLLOW_UP_STATUS_TYPE_SEEN_NOT_OK'
+                    ]));
 
-  /**
-     * Filter by slider value
-     */
-  // filterByDateRange(value: FormDateRangeSliderData) {
-  filterByDateRange(value: any) {
-    // set the new value
-    this.sliderDateFilterValue = value;
+                    // render html
+                    const html: string =
+                      `<a class="gd-list-table-link" href="${ this.location.prepareExternalUrl(url + status) }">
+                        <span is-link="${ url + status }">
+                          ${ followUpsPerDay.successfulFollowupsCount }
+                        </span>
+                      </a>
+                      ${ this.i18nService.instant('LNG_PAGE_USERS_WORKLOAD_TABLE_OF_LABEL') }
+                      <a class="gd-list-table-link" href="${ this.location.prepareExternalUrl(url) }" >
+                        <span is-link="${ url }">
+                          ${ followUpsPerDay.totalFollowupsCount }
+                        </span>
+                      </a>`;
 
-    // refresh list
-    this.needsRefreshList();
+                    // finished
+                    return html;
+                  }
+                });
+              }
+
+              // next day
+              minDate.add('1', 'days');
+            }
+          }
+
+          // update table columns
+          this.tableColumns = [
+            ...this.defaultTableColumns,
+            ...daysColumns
+          ];
+
+          // finished
+          return followUpsGroupedByUser;
+        }),
+
+        // set count
+        tap((followUpsGroupedByTeam: []) => {
+          this.pageCount = {
+            count: followUpsGroupedByTeam.length,
+            hasMore: false
+          };
+        }),
+
+        // should be the last pipe
+        takeUntil(this.destroyed$)
+      );
   }
 }
