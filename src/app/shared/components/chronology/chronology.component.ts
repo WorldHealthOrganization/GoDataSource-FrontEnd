a2a63357564f53883a3ad320dc8345561e92ce12
Adrian Morariu <adrian.morariu@clarisoft.com>
2022-08-26 12:32:48 +0300

    WGD-4053 Upgrade angular to latest possible version to fix security concerns

--- src/app/shared/components/chronology/chronology.component.ts
+++ src/app/shared/components/chronology/chronology.component.ts
@@ -1,5 +1,4 @@
 import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@angular/core';
-import * as _ from 'lodash';
 import { ChronologyItem } from './typings/chronology-item';
 import { Constants } from '../../../core/models/constants';
 import { moment } from '../../../core/helperClasses/x-moment';
@@ -21,18 +20,31 @@ export class ChronologyComponent {
     // set collection
     this._entries = entries || [];
 
-    // sort collection asc
-    this._entries = _.sortBy(
-      this._entries,
-      'date'
-    );
+    // sort collection descending
+    this._entries.sort((e1, e2) => {
+      if (e1 === e2) {
+        return 0;
+      } else if (
+        !e1 &&
+        e2
+      ) {
+        return -1;
+      } else if (
+        e1 &&
+        !e2
+      ) {
+        return 1;
+      } else {
+        return e2.date.valueOf() - e1.date.valueOf();
+      }
+    });
 
     // determine number of days between events
     let previousItem: ChronologyItem;
     this._entries.forEach((item: ChronologyItem, index: number) => {
       // we don't need to determine number of days for the first item
       if (index > 0) {
-        item.daysSincePreviousEvent = moment(item.date).startOf('day').diff(moment(previousItem.date).startOf('day'), 'days');
+        item.daysSincePreviousEvent = moment(previousItem.date).startOf('day').diff(moment(item.date).startOf('day'), 'days');
       }
 
       // previous item
