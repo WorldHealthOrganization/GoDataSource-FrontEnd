0e511b514cbbba057b2578255432bc74c1491663
Adrian Morariu <adrian.morariu@clarisoft.com>
2022-09-09 11:42:00 +0300

    WGD-3962 [Web] Audit logs page not showing "Changes"

--- src/app/core/models/audit-log.model.ts
+++ src/app/core/models/audit-log.model.ts
@@ -4,6 +4,9 @@ import { IPermissionBasic, IPermissionExportable } from './permission.interface'
 import { PERMISSION } from './permission.model';
 import { ChangeValue } from '../../shared/components-v2/app-changes-v2/models/change.model';
 
+/**
+ * Audit log changes
+ */
 export class AuditLogChangeDataModel {
   field: string;
   oldValue: any;
@@ -16,6 +19,9 @@ export class AuditLogChangeDataModel {
   }
 }
 
+/**
+ * Audit log
+ */
 export class AuditLogModel
 implements
   IPermissionBasic, IPermissionExportable {
