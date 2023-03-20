2304fbbb705abf012e997eded462617ba08ee782
Adrian Morariu <adrian.morariu@clarisoft.com>
2023-03-20 15:26:17 +0200

    WGD-3931 JQuery version

--- src/app/core/methods/mac.ts
+++ src/app/core/methods/mac.ts
@@ -2,5 +2,5 @@
  * Determine if mac device
  */
 export const determineIfMacDevice: () => boolean = () => {
-  return navigator.userAgent.toUpperCase().includes('MAC');
+  return (navigator.userAgent || '').toUpperCase().includes('MAC');
 };
