456a3d5fb00376fbec0d2cec966adc1ba2102966
Adrian Morariu <adrian.morariu@clarisoft.com>
2023-04-04 12:45:40 +0300

    WGD-4195 Sync ModusCreate GoData repositories with WHO repositories

--- src/app/i18n/english_us.ts
+++ src/app/i18n/english_us.ts
@@ -31,7 +31,6 @@ export const EnglishUsLang = {
     'LNG_PAGE_BULK_ADD_CONTACTS_LABEL_PARTIAL_ERROR_MSG': 'Records that were created with success were removed from the spreadsheet so we don\'t create duplicates'
 
 
-
     /**
      * REMOVE the tokens from below
      */
