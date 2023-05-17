e126af5fdf97e0f2a067d459bf6998ee3305e751
vonofrei-clarisoft <valeriu.onofrei@clarisoft.com>
2023-05-17 15:22:01 +0300

    WGD-3586 PRIORITY: Conversion of contacts of contacts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,9 @@ export const environment = {
   // enable Production mode
   production: false,
   // API base URL
-  apiUrl: 'http://whoapicd.clarisoft.com/api',
+  // apiUrl: 'http://whoapicd.clarisoft.com/api',
+  apiUrl: 'http://localhost:3000/api',
+  // apiUrl: 'http://192.168.100.244:3000/api',
   // log messages on server?
   enableApiLogging: false,
   // log messages in browser's console?
