bccc9d461295a10c8b458565f8d2511d29f07c2e
Adrian Morariu <adrian.morariu@clarisoft.com>
2023-05-30 12:15:04 +0300

    WGD-3370 Allow Bulk Delete of Cases and Relationships WGD-3017 Bulk Delete Cases WGD-2297 Bulk Delete of Lab Records [Service] WGD-2832 Bulk Delete of Lab Records [Web]

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,17 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import * as moment from 'moment';
+import { IData } from 'billboard.js/src/ChartInternal/data/IData';
 
 if (environment.production) {
   enableProdMode();
 }
 
-// disable deprecation warnings for now
+// disable warnings for now
 // #TODO - enable deprecation warning by removing this code once this is addressed in the entire website
 (moment as any).suppressDeprecationWarnings = true;
+// #TODO remove once proper fix found
+({} as IData);
 
 // eslint-disable-next-line no-console
 platformBrowserDynamic().bootstrapModule(AppModule).catch((err) => console.log(err));
