c43c8f247390d42cba2cdc412ab50ed665ed0ae4
Adrian Morariu <adrian.morariu@clarisoft.com>
2023-06-12 15:51:03 +0300

    WGD-3898 Improvements to filtering feature

--- src/app/core/helperClasses/request-query-builder/request-query-builder.ts
+++ src/app/core/helperClasses/request-query-builder/request-query-builder.ts
@@ -383,13 +383,13 @@ export class RequestQueryBuilder {
       }
     }
 
-    // merge "order" criterias
+    // merge "order" criteria
     this.sort.criterias = { ...this.sort.criterias, ...queryBuilder.sort.criterias };
 
     // update the "limit" if necessary
     this.limitResultsNumber = queryBuilder.limitResultsNumber || this.limitResultsNumber;
 
-    // apply pagination criterias
+    // apply pagination criteria
     if (!queryBuilder.paginator.isEmpty()) {
       this.paginator.limit = queryBuilder.paginator.limit;
       this.paginator.skip = queryBuilder.paginator.skip;
