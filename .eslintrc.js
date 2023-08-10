468e70fae45d5e1667e7745b8725cce2c5c199a0
Adrian Morariu <adrian.morariu@moduscreate.com>
2023-08-10 16:04:52 +0300

    WGD-3840 Modify option sets for Contact Final Follow-Up Status (Ref data)

--- .eslintrc.js
+++ .eslintrc.js
@@ -144,7 +144,7 @@ module.exports = {
         "max-len": [
           "error",
           {
-            "code": 512
+            "code": 1024
           }
         ],
         "no-bitwise": "error",
