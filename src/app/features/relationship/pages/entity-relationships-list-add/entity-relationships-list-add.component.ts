4a3128e44119857a4b00e11e240f8450e18615fc
vonofrei-clarisoft <valeriu.onofrei@clarisoft.com>
2023-08-22 18:23:03 +0300

    WGD-4259 [Web] Cases with Classification: "Not a case" should not be in available people

--- src/app/features/relationship/pages/entity-relationships-list-add/entity-relationships-list-add.component.ts
+++ src/app/features/relationship/pages/entity-relationships-list-add/entity-relationships-list-add.component.ts
@@ -17,10 +17,7 @@ import { TopnavComponent } from '../../../../core/components/topnav/topnav.compo
 import { RelationshipType } from '../../../../core/enums/relationship-type.enum';
 import { EntityHelperService } from '../../../../core/services/helper/entity-helper.service';
 import { IV2ColumnPinned, IV2ColumnStatusFormType, V2ColumnFormat, V2ColumnStatusForm } from '../../../../shared/components-v2/app-list-table-v2/models/column.model';
-import {
-  V2FilterType,
-  V2FilterTextType
-} from '../../../../shared/components-v2/app-list-table-v2/models/filter.model';
+import { V2FilterType, V2FilterTextType } from '../../../../shared/components-v2/app-list-table-v2/models/filter.model';
 import { IResolverV2ResponseModel } from '../../../../core/services/resolvers/data/models/resolver-response.model';
 import { V2AdvancedFilterType } from '../../../../shared/components-v2/app-list-table-v2/models/advanced-filter.model';
 import { V2ActionType } from '../../../../shared/components-v2/app-list-table-v2/models/action.model';
